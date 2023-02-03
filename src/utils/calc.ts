9ba19773c735c5f48e11e04720192b9857227053
KristhDev <kristhdev@gmail.com>
2023-02-02 22:52:31 -0600

    Docs: add documentation in utils

--- src/utils/calc.ts
+++ src/utils/calc.ts
@@ -1,9 +1,20 @@
 import dayjs from 'dayjs';
 
+/**
+ * It takes an array of numbers and returns the sum of all the numbers in the array.
+ * @param {number[]} numbers - number[] - an array of numbers
+ * @returns The sum of the numbers in the array.
+ */
 export const sumNumbers = (numbers: number[]) => {
     return numbers.reduce((total, number) => total + number, 0);
 }
 
+/**
+ * It takes an array of objects with two properties, init and finish, and returns the sum of the hours
+ * between the two dates
+ * @param {{ init: string, finish: string }[]} dates - { init: string, finish: string }[]
+ * @returns The sum of the hours between the dates.
+ */
 export const sumHours = (dates: { init: string, finish: string }[]) => {
     const hours = dates.map(date => {
         const start = dayjs(date.init);
@@ -19,12 +30,24 @@ export const sumHours = (dates: { init: string, finish: string }[]) => {
         : sumNumbers(hours);
 }
 
+/**
+ * It takes an array of objects with two properties, init and finish, and returns the sum of the
+ * difference between the init and finish properties.
+ * @param {{ init: string, finish: string }[]} dates - { init: string, finish: string }[]
+ * @returns The restMins value from the sumMins function.
+ */
 export const getRestMins = (dates: { init: string, finish: string }[]) => {
     const { restMins } = sumMins(dates);
 
     return restMins;
 }
 
+/**
+ * It takes an array of objects with two properties, init and finish, and returns an object with two
+ * properties, hours and restMins
+ * @param {{ init: string, finish: string }[]} dates - { init: string, finish: string }[]
+ * @returns An object with two properties, hours and restMins
+ */
 export const sumMins = (dates: { init: string, finish: string }[]) => {
     const mins = dates.map(date => {
         const start = dayjs(date.init, 'HH:mm');
