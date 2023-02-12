541b8dcb561f5aff169610b9535d2ef8f7528267
KristhDev <kristhdev@gmail.com>
2023-02-11 18:52:51 -0600

    Test: add testing setup

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+    preset: 'react-native',
+    setupFilesAfterEnv: [ '@testing-library/jest-native/extend-expect' ],
+    // setupFiles: [ './jest.setup.js' ],
+    transformIgnorePatterns: [],
+    testMatch: [
+        '**/__tests__/**',
+        '!**/src/**',
+        '!**/__snapshots__/**'
+    ]
+};
+
+export default config;
