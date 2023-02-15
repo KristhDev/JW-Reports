22c8f07dc16e26008a00cd573affb2f4c8d654d1
KristhDev <kristhdev@gmail.com>
2023-02-15 09:57:54 -0600

    Feat: add push notifications

--- App.tsx
+++ App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'reduxjs-toolkit-persist/lib/integration/react';
 import { NavigationContainer, useNavigationContainerRef } from '@react-navigation/native';
 import { useFlipper } from '@react-navigation/devtools';
 import { MenuProvider } from 'react-native-popup-menu';
 import { Provider as PaperProvider } from 'react-native-paper';
+import OneSignal from 'react-native-onesignal';
 import dayjs from 'dayjs';
 import 'dayjs/locale/es';
+
+/* Env */
+import { ONESIGNAL_APP_ID } from '@env';
 
 /* Features */
 import store, { persistor } from './src/features/store';
@@ -28,6 +32,27 @@ const App = () => {
   const navigationRef = useNavigationContainerRef();
 
   useFlipper(navigationRef);
+
+  /**
+   * Effect to initialize the OneSignal SDK and
+   * listen for push notifications.
+   */
+  useEffect(() => {
+    ONESIGNAL_APP_ID;
+    OneSignal.setLogLevel(6, 0);
+    OneSignal.setAppId(ONESIGNAL_APP_ID);
+
+    /**
+     * A method that shows a native prompt to the user asking for permission to send
+     * notificationspush notifications.
+     */
+    OneSignal.promptForPushNotificationsWithUserResponse();
+
+    OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+      const notification = notificationReceivedEvent.getNotification();
+      notificationReceivedEvent.complete(notification);
+    });
+  }, []);
 
   return (
     <MenuProvider>
--- env.d.ts
+++ env.d.ts
@@ -5,4 +5,5 @@ declare module '@env' {
     export const SUPABASE_BUCKET: string;
     export const SUPABASE_REVISITS_FOLDER: string;
     export const SUPABASE_URL: string;
+    export const ONESIGNAL_APP_ID: string;
 }
