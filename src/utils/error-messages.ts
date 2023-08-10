f11647b0c5ca1a6911d73365f19fa9c66bcc505a
KristhDev <kristhdev@gmail.com>
2023-08-09 23:00:40 -0600

    Feat: add new message un error-messages

--- src/utils/error-messages.ts
+++ src/utils/error-messages.ts
@@ -9,6 +9,7 @@ export const authErrorMessages = {
     'Unable to validate email address: invalid format': 'El formato del correo electrónico no es válido.',
     'Password should be at least 6 characters': 'La contraseña debe tener al menos 6 caracteres.',
     'Invalid Refresh Token: Refresh Token Not Found': 'Su tiempo de sesión ha expirado.',
+    'Invalid Refresh Token: Already Used': 'Su tiempo de sesión ha expirado.',
     'A user with this email address has already been registered': 'Ya existe un usuario con este correo electrónico.',
     'Auth session missing!': 'Para realizar está acción debes iniciar sesión.',
     'Password recovery requires an email': 'La recuperación de contraseña requiere un correo electrónico.',
