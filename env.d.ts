35504973c3b5807d61d0ea50d748658c81f1577a
KristhDev <kristhdev@gmail.com>
2023-11-14 20:43:28 -0600

    Chore: add env in module @env

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,11 @@
 declare module '@env' {
     export const APP_VERSION: string;
+    export const ONESIGNAL_APP_ID: string;
     export const REPOSITORY_URL: string;
     export const SITIE_URL: string;
     export const SUPABASE_APY_KEY: string;
     export const SUPABASE_BUCKET: string;
     export const SUPABASE_REVISITS_FOLDER: string;
+    export const SUPABASE_SERVICE_ROLE_KEY: string;
     export const SUPABASE_URL: string;
-    export const ONESIGNAL_APP_ID: string;
 }
