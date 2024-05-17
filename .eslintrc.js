62e7a88608a64cd2e24ef50a0c99e58a4a0e65d1
KristhDev <kristhdev@gmail.com>
2024-05-16 20:54:59 -0600

    Test(setup): add eslint plugin for testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,11 @@ module.exports = {
     'react-native/no-inline-styles': 0,
     'react/no-unstable-nested-components': 0,
     semi: 0
-  }
+  },
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: ['plugin:testing-library/react']
+    }
+  ]
 };
--- src/modules/theme/hooks/useTheme.ts
+++ src/modules/theme/hooks/useTheme.ts
@@ -2,13 +2,14 @@ import { useContext } from 'react';
 
 /* Context */
 import { ThemeContext } from '../context';
+import { ThemeContextProps } from '../interfaces';
 
 /**
  * A hook that returns the current theme context.
  *
- * @return {ThemeContext} The current theme context.
+ * @return {ThemeContextProps} The current theme context.
  */
-const useTheme = () => {
+const useTheme = (): ThemeContextProps => {
     return useContext(ThemeContext);
 }
 
