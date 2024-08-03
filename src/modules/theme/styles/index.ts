92c9bafd5984126b13d9e3b11636c7dc2b4adb4a
KristhDev <kristhdev@gmail.com>
2024-08-03 12:00:09 -0600

    Feat(theme): add border radius to theme

--- src/modules/theme/styles/index.ts
+++ src/modules/theme/styles/index.ts
@@ -1,5 +1,6 @@
 export { default as styles } from './styles';
 export * from './breakpoints';
+export * from './border-radius';
 export * from './colors';
 export * from './font-sizes';
 export * from './margins';
