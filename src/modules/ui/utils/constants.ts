4d8ca82fecd9581bfbedab342bfc772cc86adf03
KristhDev <kristhdev@gmail.com>
2024-08-04 20:52:19 -0600

    Feat(ui): add constants and functions in utils

---
+++ src/modules/ui/utils/constants.ts
@@ -0,0 +1,6 @@
+import { ItemOption } from '../interfaces';
+
+export const UI_OPTIONS: ItemOption[] = [
+    { label: 'Habilitar', value: true },
+    { label: 'Deshabilitar', value: false },
+];
---
+++ src/modules/ui/utils/functions.ts
@@ -0,0 +1,23 @@
+/* Interfaces */
+import { UserInterface } from '../interfaces';
+
+/* utils */
+import { storage, storageKeys } from '../../../utils';
+
+export const getUIStored = (): UserInterface => {
+    let initialUserInterface: UserInterface = {
+        oldDatetimePicker: false
+    }
+
+    const userInterfaceStored = storage.getItem(storageKeys.USER_INTERFACE);
+    if (!userInterfaceStored) return initialUserInterface;
+
+    const value = JSON.parse(userInterfaceStored) as UserInterface;
+    if (!value) return initialUserInterface;
+
+    if (value.oldDatetimePicker) {
+        initialUserInterface.oldDatetimePicker = value.oldDatetimePicker;
+    }
+
+    return initialUserInterface;
+}
