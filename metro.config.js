c3c0c1b9873bd7e621a63885801988d2c6e54702
KristhDev <kristhdev@gmail.com>
2024-08-15 00:03:57 -0600

    Build: add reset cache in metro config

--- metro.config.js
+++ metro.config.js
@@ -6,6 +6,8 @@ const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+    resetCache: true
+}
 
 module.exports = mergeConfig(getDefaultConfig(__dirname), config);
