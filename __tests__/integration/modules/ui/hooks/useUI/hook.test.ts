26b8a649c5c5321a56a6471b44baa50b79c65759
KristhDev <kristhdev@gmail.com>
2024-08-27 18:44:58 -0600

    Test(integration): add test in structure of useUI hook

---
+++ __tests__/integration/modules/ui/hooks/useUI/hook.test.ts
@@ -0,0 +1,29 @@
+/* Setups */
+import { getMockStoreUseUI, renderUseUI } from '../../../../../setups';
+
+/* Mocks */
+import { initialUIState } from '../../../../../mocks';
+
+describe('Test in useUI hook', () => {
+    let mockStore = {} as any;
+
+    beforeEach(() => {
+        jest.clearAllMocks();
+
+        mockStore = getMockStoreUseUI({
+            ui: initialUIState
+        });
+    });
+
+    it('should return respective properties and functions', () => {
+        const { result } = renderUseUI(mockStore);
+
+        expect(result.current.useUI).toEqual({
+            state: initialUIState,
+
+            listenHideKeyboard: expect.any(Function),
+            listenShowKeyboard: expect.any(Function),
+            setOldDatetimePicker: expect.any(Function)
+        });
+    });
+});
