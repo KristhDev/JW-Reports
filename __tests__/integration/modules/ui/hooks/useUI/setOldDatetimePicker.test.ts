395d71bc41e7001cb24726b30973312f018e0f3e
KristhDev <kristhdev@gmail.com>
2024-08-27 18:45:51 -0600

    Test(integration): add test in setOldDatetimePicker of useUI

---
+++ __tests__/integration/modules/ui/hooks/useUI/setOldDatetimePicker.test.ts
@@ -0,0 +1,33 @@
+import { act } from '@testing-library/react-native';
+
+/* Setups */
+import { getMockStoreUseUI, renderUseUI } from '../../../../../setups';
+
+/* Mocks */
+import { initialUIState, storageSpy } from '../../../../../mocks';
+
+describe('Test in setOldDatetimePicker', () => {
+    it('should change property oldDatetimePicker', async () => {
+        storageSpy.getItem.mockImplementation();
+
+        const mockStore = getMockStoreUseUI({
+            ui: initialUIState
+        });
+
+        const { result } = renderUseUI(mockStore);
+
+        expect(result.current.useUI.state.userInterface).toEqual({
+            ...initialUIState.userInterface,
+            oldDatetimePicker: false
+        });
+
+        await act(() => {
+            result.current.useUI.setOldDatetimePicker(true);
+        });
+
+        expect(result.current.useUI.state.userInterface).toEqual({
+            ...result.current.useUI.state.userInterface,
+            oldDatetimePicker: true
+        });
+    });
+});
