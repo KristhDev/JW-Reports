a29d39bf5ff09b6608b90fdc6aafcbebdb200190
KristhDev <kristhdev@gmail.com>
2024-09-02 16:51:41 -0600

    Build: add alias in babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,6 @@
-module.exports = {
+const path = require('path');
+
+const babelConfig = {
   presets: [ 'module:@react-native/babel-preset' ],
   plugins: [
     [ 'module:react-native-dotenv', {
@@ -13,6 +15,32 @@ module.exports = {
       'allowUndefined': true,
       'verbose': false
     } ],
+    [ 'module-resolver', {
+      extensions: ['.ios.js', '.android.js', '.ios.jsx', '.android.jsx', '.js', '.jsx', '.json', '.ts', '.tsx'],
+      root: ['.'],
+      alias: {
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@auth': path.resolve(__dirname, './src/modules/auth'),
+        '@config': path.resolve(__dirname, './src/config'),
+        '@courses': path.resolve(__dirname, './src/modules/courses'),
+        '@features': path.resolve(__dirname, './src/features'),
+        '@lessons': path.resolve(__dirname, './src/modules/lessons'),
+        '@mocks': path.resolve(__dirname, './__tests__/mocks'),
+        '@navigation': path.resolve(__dirname, './src/navigation'),
+        '@package': path.resolve(__dirname, './package.json'),
+        '@preaching': path.resolve(__dirname, './src/modules/preaching'),
+        '@providers': path.resolve(__dirname, './src/providers'),
+        '@revisits': path.resolve(__dirname, './src/modules/revisits'),
+        '@services': path.resolve(__dirname, './src/services'),
+        '@setups': path.resolve(__dirname, './__tests__/setups'),
+        '@shared': path.resolve(__dirname, './src/modules/shared'),
+        '@test-config': path.resolve(__dirname, './__tests__/config'),
+        '@test-setup': path.resolve(__dirname, './jest.setup.ts'),
+        '@theme': path.resolve(__dirname, './src/modules/theme'),
+        '@ui': path.resolve(__dirname, './src/modules/ui'),
+        '@utils': path.resolve(__dirname, './src/utils'),
+      }
+    } ],
     [
       'react-native-reanimated/plugin',
       {
@@ -20,4 +48,6 @@ module.exports = {
       }
     ]
   ]
-};
+}
+
+module.exports = babelConfig;
