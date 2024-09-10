ec943e8c7d1c92a89067a12849861b6a4d3f82ce
KristhDev <kristhdev@gmail.com>
2024-09-09 22:53:27 -0600

    Build: add emailjs envs

--- env.d.ts
+++ env.d.ts
@@ -1,5 +1,8 @@
 declare module '@env' {
     export const BUGFENDER_API_KEY: string;
+    export const EMAILJS_FEEDBACK_TEMPLATE_ID: string;
+    export const EMAILJS_PUBLIC_KEY: string;
+    export const EMAILJS_SERVICE_ID: string;
     export const REPOSITORY_URL: string;
     export const SITIE_URL: string;
     export const SUPABASE_APY_KEY: string;
