7baf0c4f8efe22108b4b73684f1e45a2e8e9ed7c
KristhDev <kristhdev@gmail.com>
2024-09-10 14:32:58 -0600

    Feat(preaching): add constants util

--- src/modules/preaching/utils/constants.ts
+++ src/modules/preaching/utils/constants.ts
@@ -14,3 +14,15 @@ export const HOURS_REQUIREMENTS = {
     'regular': 50,
     'especial': 90
 }
+
+export const preachingMessages = {
+    ADDED_SUCCESS: 'Has agregado tu día de predicación correctamente.',
+    DAY_REQUIRED: 'El día no puede estar vacío.',
+    DELETED_SUCCESS: 'Has eliminado tu día de predicación correctamente.',
+    FINAL_HOUR_REQUIRED: 'La hora final no puede estar vacía.',
+    INIT_HOUR_GREATER_THAN_FINAL: 'La hora inicial no puede ser mayor que la hora final.',
+    INIT_HOUR_REQUIRED: 'La hora inicial no puede estar vacía.',
+    UNSELECTED_DELETE: 'No hay un dia de predicación seleccionado para eliminar.',
+    UNSELECTED_UPDATE: 'No hay un dia de predicación seleccionado para actualizar.',
+    UPDATED_SUCCESS: 'Has actualizado tu día de predicación correctamente.',
+}
