03f2eca699870c29ad170cc19221e5a6398d1dbd
KristhDev <kristhdev@gmail.com>
2024-09-10 14:12:27 -0600

    Build: add EMAILJS_REPORT_ERROR_TEMPLATE_ID and SUPABASE_ERRORS_FOLDER

--- env.d.ts
+++ env.d.ts
@@ -2,12 +2,14 @@ declare module '@env' {
     export const BUGFENDER_API_KEY: string;
     export const EMAILJS_FEEDBACK_TEMPLATE_ID: string;
     export const EMAILJS_PUBLIC_KEY: string;
+    export const EMAILJS_REPORT_ERROR_TEMPLATE_ID: string;
     export const EMAILJS_SERVICE_ID: string;
     export const REPOSITORY_URL: string;
     export const SITIE_URL: string;
     export const SUPABASE_APY_KEY: string;
     export const SUPABASE_BUCKET: string;
     export const SUPABASE_REVISITS_FOLDER: string;
+    export const SUPABASE_ERRORS_FOLDER: string;
     export const SUPABASE_URL: string;
     export const ONESIGNAL_APP_ID: string;
 }
