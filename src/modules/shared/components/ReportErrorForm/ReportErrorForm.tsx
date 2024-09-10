87e872cdd4e044d11a4cb5b4bd6c0af5f02408e8
KristhDev <kristhdev@gmail.com>
2024-09-10 17:03:41 -0600

    Feat(shared): add functionality to send report error email

--- src/modules/shared/components/ReportErrorForm/ReportErrorForm.tsx
+++ src/modules/shared/components/ReportErrorForm/ReportErrorForm.tsx
@@ -1,31 +1,64 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { ActivityIndicator, View } from 'react-native';
 import { Image } from 'react-native-image-crop-picker';
 import { Formik } from 'formik';
 import { useStyles } from 'react-native-unistyles';
 
-import { Button, FormField, FormImage } from '@ui';
+/* Components */
+import { Button, FormField, FormImage, FormImageRef } from '@ui';
 
-import { useStatus } from '../../hooks';
+/* Hooks */
+import { useEmail, useStatus } from '../../hooks';
 
+/* Schemas */
 import { reportErrorFormSchema } from './schemas';
 
-import { useTheme, themeStylesheet } from '@theme';
+/* Interfaces */
+import { FormActions } from '../../interfaces';
 
+/* Utils */
 import { reportErrorDefaultImgs } from '../../utils';
 
+/* Styles */
+import { useTheme, themeStylesheet } from '@theme';
+
+/**
+ * This component is responsible for rendering the form to report an error.
+ *
+ * @return {JSX.Element} The rendered form component.
+ */
 export const ReportErrorForm = (): JSX.Element => {
+    const formImageRef = useRef<FormImageRef>(null);
     const [ image, setImage ] = useState<Image | null>(null);
 
     const { styles: themeStyles, theme: { colors, fontSizes, margins } } = useStyles(themeStylesheet);
 
+    const { sendReportErrorEmail } = useEmail();
     const { setErrorForm } = useStatus();
     const { state: { theme } } = useTheme();
+
+    /**
+     * Handles the send report error email functionality.
+     *
+     * @param {string} message - The message to send in the email.
+     * @param {{ resetForm: () => void, setSubmitting: (isSubmitting: boolean) => void }} formActions - The actions to call when the email is sent.
+     * @return {void} This function does not return any value.
+     */
+    const handleSendReportErrorEmail = (message: string, { resetForm, setSubmitting }: FormActions): void => {
+        sendReportErrorEmail({ message, image }, {
+            onFinish: () => setSubmitting && setSubmitting(false),
+            onSuccess: (): void => {
+                resetForm && resetForm();
+                setImage(null);
+                formImageRef.current?.clearImage();
+            }
+        });
+    }
 
     return (
         <Formik
             initialValues={{ message: '' }}
-            onSubmit={ () => {} }
+            onSubmit={ ({ message }, { resetForm, setSubmitting }) => handleSendReportErrorEmail(message, { resetForm, setSubmitting }) }
             validateOnMount
             validationSchema={ reportErrorFormSchema }
         >
@@ -45,6 +78,7 @@ export const ReportErrorForm = (): JSX.Element => {
                         galleryButtonText="Añadir imagen"
                         label="Adjunte una imagen (opcional):"
                         onSelectImage={ setImage }
+                        ref={ formImageRef }
                         showGalleryButton
                         style={{ marginBottom: margins.xl }}
                     />
