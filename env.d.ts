bda8e008be206370144620acb267be8362b35362
KristhDev <kristhdev@gmail.com>
2024-09-18 10:36:12 -0600

    Refactor(env): add old env in env.d.ts

--- env.d.ts
+++ env.d.ts
@@ -4,12 +4,13 @@ declare module '@env' {
     export const EMAILJS_PUBLIC_KEY: string;
     export const EMAILJS_REPORT_ERROR_TEMPLATE_ID: string;
     export const EMAILJS_SERVICE_ID: string;
+    export const ONESIGNAL_APP_ID: string;
     export const REPOSITORY_URL: string;
     export const SITIE_URL: string;
     export const SUPABASE_APY_KEY: string;
     export const SUPABASE_BUCKET: string;
-    export const SUPABASE_REVISITS_FOLDER: string;
     export const SUPABASE_ERRORS_FOLDER: string;
+    export const SUPABASE_REVISITS_FOLDER: string;
+    export const SUPABASE_SERVICE_ROLE_KEY: string;
     export const SUPABASE_URL: string;
-    export const ONESIGNAL_APP_ID: string;
 }
