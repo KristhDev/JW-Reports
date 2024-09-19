50c50c6041dd442b885437b04e13c501583acd5d
KristhDev <kristhdev@gmail.com>
2024-09-18 19:45:19 -0600

    Test(unit): add test in constants util of auth

--- __tests__/unit/modules/auth/utils/constants.test.ts
+++ __tests__/unit/modules/auth/utils/constants.test.ts
@@ -1,7 +1,15 @@
-import { PRECURSORS_OPTIONS } from '@auth';
+import { authMessages, precursorMessages, PRECURSORS_OPTIONS } from '@auth';
 
 describe('Test in constants of auth module', () => {
     it('should to match snapshot - PRECURSORS_OPTIONS', () => {
         expect(PRECURSORS_OPTIONS).toMatchSnapshot();
+    });
+
+    it('should to match snapshot - authMessages', () => {
+        expect(authMessages).toMatchSnapshot();
+    });
+
+    it('should to match snapshot - precursorMessages', () => {
+        expect(precursorMessages).toMatchSnapshot();
     });
 });
