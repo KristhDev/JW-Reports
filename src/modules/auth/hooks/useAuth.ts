c474d4656ae8bc2274fa73a299a432ee412b13b2
KristhDev <kristhdev@gmail.com>
2024-09-24 21:04:11 -0600

    Refactor(auth): replace refresh_token by access_token

--- src/modules/auth/hooks/useAuth.ts
+++ src/modules/auth/hooks/useAuth.ts
@@ -62,7 +62,7 @@ const useAuth = () => {
         if (next) return;
 
         dispatch(setUserAction({
-            token: session?.refresh_token!,
+            token: session?.access_token!,
             user: userAdpater({
                 ...user?.user_metadata,
                 id: user?.id!,
@@ -86,7 +86,7 @@ const useAuth = () => {
             return;
         }
 
-        const user = await supabase.auth.getUser();
+        const user = await supabase.auth.getUser(state.token);
         const session = await supabase.auth.getSession();
 
         const response: AuthResponse = {
