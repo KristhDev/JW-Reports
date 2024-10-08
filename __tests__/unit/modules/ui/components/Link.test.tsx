30586ac63a08a408acb42d814e7f7ba438bb6b01
KristhDev <kristhdev@gmail.com>
2024-10-08 08:01:01 -0600

    Test(unit): add test in Link component

---
+++ __tests__/unit/modules/ui/components/Link.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, screen, userEvent } from '@testing-library/react-native';
+
+/* Setup */
+import { onPressMock } from '@test-setup';
+
+/* Components */
+import { Link, LinkProps } from '@ui';
+
+const renderComponent = (props: LinkProps) => render(<Link { ...props } />);
+const user = userEvent.setup();
+
+describe('Test in <Link /> component', () => {
+    it('should to match snapshot', () => {
+        renderComponent({
+            children: 'Test text',
+            onPress: onPressMock,
+        });
+
+        expect(screen.toJSON()).toMatchSnapshot();
+    });
+
+    it('should render respective props', () => {
+        renderComponent({
+            children: 'Test text',
+            onPress: onPressMock,
+            testID: 'link-test',
+        });
+
+        /* Get elements with props of component */
+        const text = screen.queryByText('Test text');
+        const touchable = screen.queryByTestId('link-test');
+
+        /* Check if exists this elemets and has props */
+        expect(text).toBeOnTheScreen();
+        expect(text).toHaveTextContent('Test text');
+
+        expect(touchable).toBeOnTheScreen();
+        expect(touchable).toHaveProp('testID', 'link-test');
+    });
+
+    it('should call onPress when pressed', async () => {
+        renderComponent({
+            children: 'Test text',
+            onPress: onPressMock,
+        });
+
+        /* Get pressable */
+        const pressable = screen.getByTestId('link-touchable');
+        await user.press(pressable);
+
+        /* Check if onPress is called one time */
+        expect(onPressMock).toHaveBeenCalledTimes(1);
+    });
+});
