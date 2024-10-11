f18160bd6936a940469ec62ff1148263f88aff49
KristhDev <kristhdev@gmail.com>
2024-10-11 16:00:42 -0600

    Refactor: sort env of project

--- env.d.ts
+++ env.d.ts
@@ -8,8 +8,8 @@ declare module '@env' {
     export const SITIE_URL: string;
     export const SUPABASE_APY_KEY: string;
     export const SUPABASE_BUCKET: string;
-    export const SUPABASE_REVISITS_FOLDER: string;
     export const SUPABASE_ERRORS_FOLDER: string;
+    export const SUPABASE_REVISITS_FOLDER: string;
     export const SUPABASE_URL: string;
     export const ONESIGNAL_APP_ID: string;
 }
