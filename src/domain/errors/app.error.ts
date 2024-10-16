ca664457405bfda97b93fe6970c0804af1ad9305
KristhDev <kristhdev@gmail.com>
2024-10-16 12:47:36 -0600

    Refactor(errors): fix error codes in AppErrors

--- src/domain/errors/app.error.ts
+++ src/domain/errors/app.error.ts
@@ -1,24 +1,28 @@
 export class AppErrors {
-    public static supabaseAuthErrors = {
-        'Invalid login credentials': 'Las credenciales no son válidas.',
-        'Unable to validate email address: invalid format': 'El formato del correo electrónico no es válido.',
-        'Password should be at least 6 characters': 'La contraseña debe tener al menos 6 caracteres.',
-        'Invalid Refresh Token: Refresh Token Not Found': 'Su tiempo de sesión ha expirado.',
-        'Invalid Refresh Token: Already Used': 'Su tiempo de sesión ha expirado.',
-        'A user with this email address has already been registered': 'Ya existe un usuario con este correo electrónico.',
-        'Auth session missing!': 'Para realizar está acción debes iniciar sesión.',
-        'Password recovery requires an email': 'La recuperación de contraseña requiere un correo electrónico.',
-        'For security purposes, you can only request this once every 60 seconds': 'Para evitar problemas de seguridad, solo puede realizar esta solicitud una vez cada 60 segundos.'
+    public static supabaseAuthCodeErrors = {
+        'bad_code_verifier': 'El código de autorización es incorrecto.',
+        'bad_json': 'El formato de la información no es válido.',
+        'bad_jwt': 'Su tiempo de sesión ha expirado.',
+        'email_address_not_authorized': 'Este correo electrónico no se encuentra autorizado.',
+        'email_conflict_identity_not_deletable': 'El correo electrónico ya se encuentra registrado.',
+        'email_exists': 'El correo electrónico ya se encuentra registrado.',
+        'email_not_confirmed': 'El correo electrónico no ha sido confirmado.',
+        'email_provider_disabled': 'El proveedor de correo electrónico ha sido deshabilitado.',
+        'flow_state_expired': 'Su tiempo de sesión ha expirado.',
+        'invalid_credentials': 'Las credenciales son inválidas.',
+        'no_authorization': 'Debe iniciar sesión para realizar esta acción.',
+        'not_admin': 'No se puede realizar está acción',
+        'same_password': 'No puede utilizar la contraseña actual como nueva.',
+        'session_not_found': 'Su sesión no existe.',
+        'sms_send_failed': 'No se pudo enviar el correo de verificación, por favor contacte con el administrador.',
+        'user_already_exists': 'El usuario ya existe.',
+        'user_banned': 'El usuario ha sido baneado.',
+        'user_not_found': 'El usuario no existe.',
+        'validation_failed': 'El formato de la información no es válido.',
     }
 
-    public static supabaseCommonErrors = {
-        'Network request failed': 'Lo sentimos pero no dispones de conexión a internet.',
-        'FetchError: Network request failed': 'Lo sentimos pero no dispones de conexión a internet.'
-    }
-
-    public static translateMsg = (message: string): string => {
-        return (AppErrors.supabaseAuthErrors as any)[message] ||
-            (AppErrors.supabaseCommonErrors as any)[message] ||
-            'Ocurrió un error al realizar está acción, por favor vuelvalo a intentar.';
+    public static translateMsg = (code: string): string => {
+        return (AppErrors.supabaseAuthCodeErrors as any)[code]  ||
+        'Ocurrió un error al realizar está acción, por favor vuelvalo a intentar.';
     }
 }
