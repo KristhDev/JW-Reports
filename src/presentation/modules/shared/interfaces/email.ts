8788b0ddbab26db06df1219eaf0a2742ab2eefb0
KristhDev <kristhdev@gmail.com>
2024-10-25 19:46:32 -0600

    Refactor(shared): remove SendEmailOptions interface

--- src/presentation/modules/shared/interfaces/email.ts
+++ src/presentation/modules/shared/interfaces/email.ts
@@ -1,12 +1,5 @@
 import { ImageModel } from '@domain/models';
 
-export interface SendEmailOptions {
-    email: string;
-    imageUrl?: string;
-    message: string;
-    templateId: string;
-}
-
 export interface ReportErrorOptions {
     image: ImageModel | null;
     message: string;
