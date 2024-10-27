7ecb45580d8d9fd0289443a95a63938f3b93163b
KristhDev <kristhdev@gmail.com>
2024-10-26 22:21:39 -0600

    Test(unit): fix unit tests

--- __tests__/unit/infrastructure/adapters/device-info.test.ts
+++ __tests__/unit/infrastructure/adapters/device-info.test.ts
@@ -1,3 +1,6 @@
+import DeviceInfoRN from 'react-native-device-info';
+
+/* Adapters */
 import { DeviceInfo } from '@infrasturcture/adapters';
 
 describe('Test in DeviceInfo adapter', () => {
@@ -9,11 +12,15 @@ describe('Test in DeviceInfo adapter', () => {
     });
 
     it('should get build version - getBuildVersion', () => {
+        (DeviceInfoRN.getBuildNumber as jest.Mock).mockReturnValue('9102');
+
         const result = DeviceInfo.getBuildVersion();
         expect(result).toBe('9102');
     });
 
     it('should get system version - getSystemVersion', () => {
+        (DeviceInfoRN.getSystemVersion as jest.Mock).mockReturnValue('12');
+
         const result = DeviceInfo.getSystemVersion();
         expect(result).toBe('12');
     });
