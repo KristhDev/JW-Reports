c355b6bfd5439527a537461591b97a0a51db379d
KristhDev <kristhdev@gmail.com>
2024-11-04 17:06:58 -0600

    Feat(entities): add CourseWithLessonsEntity

--- src/domain/entities/course.entity.ts
+++ src/domain/entities/course.entity.ts
@@ -41,3 +41,41 @@ export class CourseEntity {
         );
     }
 }
+
+export class CourseWithLessonsEntity {
+    constructor(
+        public readonly id: string,
+        public readonly userId: string,
+        public readonly personName: string,
+        public readonly personAbout: string,
+        public readonly personAddress: string,
+        public readonly publication: string,
+        public readonly lessons: LessonEntity[],
+        public readonly suspended: boolean,
+        public readonly finished: boolean,
+        public readonly createdAt: string,
+        public readonly updatedAt: string,
+    ) {}
+
+    /**
+     * Converts a CourseEndpoint object to a CourseWithLessons object.
+     *
+     * @param {CourseEndpoint} course - The course data from the endpoint
+     * @returns {CourseWithLessonsEntity} A new CourseWithLessons object
+     */
+    public static fromEndpoint(course: CourseEndpoint): CourseWithLessonsEntity {
+        return new CourseWithLessonsEntity(
+            course.id,
+            course.user_id,
+            course.person_name,
+            course.person_about,
+            course.person_address,
+            course.publication,
+            (course?.lessons) ? course.lessons.map(LessonEntity.fromEndpoint) : [],
+            course.suspended,
+            course.finished,
+            course.created_at,
+            course.updated_at
+        );
+    }
+}
