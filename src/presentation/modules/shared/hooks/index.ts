9023a668660a70faf57f1ea8f578ff0a8ab1035e
KristhDev <kristhdev@gmail.com>
2024-11-08 16:23:15 -0600

    Feat(shared): add useExportData hook

--- src/presentation/modules/shared/hooks/index.ts
+++ src/presentation/modules/shared/hooks/index.ts
@@ -1,4 +1,5 @@
 export { default as useEmail } from './useEmail';
+export { default as useExportData } from './useExportData';
 export { default as useImage } from './useImage';
 export { default as useNetwork } from './useNetwork';
 export { default as usePermissions } from './usePermissions';
