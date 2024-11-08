8b11625994579b23695165b56a1dbccf5fa9b10c
KristhDev <kristhdev@gmail.com>
2024-11-08 16:18:37 -0600

    Feat(interfaces): add PreachingsTemplateOptions interface

--- src/infrasturcture/interfaces/templates.interfaces.ts
+++ src/infrasturcture/interfaces/templates.interfaces.ts
@@ -1,11 +1,17 @@
+import { PreachingReportModel } from '@domain/models';
 import { RevisitEntity, CourseWithLessonsEntity } from '@domain/entities';
-
-export interface RevisitsTemplateOptions {
-    fullName: string;
-    revisits: RevisitEntity[];
-}
 
 export interface CoursesTemplateOptions {
     fullName: string;
     courses: CourseWithLessonsEntity[];
+}
+
+export interface PreachingsTemplateOptions {
+    fullName: string;
+    reports: PreachingReportModel[];
+}
+
+export interface RevisitsTemplateOptions {
+    fullName: string;
+    revisits: RevisitEntity[];
 }
