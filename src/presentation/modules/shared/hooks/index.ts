8911331166850c60729ea306ad180654278e7622
KristhDev <kristhdev@gmail.com>
2024-11-09 14:38:19 -0600

    Feat(shared): add useVoiceRecorder hook

--- src/presentation/modules/shared/hooks/index.ts
+++ src/presentation/modules/shared/hooks/index.ts
@@ -4,3 +4,4 @@ export { default as useImage } from './useImage';
 export { default as useNetwork } from './useNetwork';
 export { default as usePermissions } from './usePermissions';
 export { default as useStatus } from './useStatus';
+export { default as useVoiceRecorder } from './useVoiceRecorder';
