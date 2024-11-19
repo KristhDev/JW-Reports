954a16db4073e917273a319bb84d4bd07ca7b4de
KristhDev <kristhdev@gmail.com>
2024-11-19 08:13:26 -0600

    Test(unit): add test in PdfCoursesTemplate template

--- __tests__/unit/domain/templates/pdf-courses.template.test.ts
+++ __tests__/unit/domain/templates/pdf-courses.template.test.ts
@@ -1,3 +1,6 @@
+/* Mocks */
+import { coursesWithLessonsMock } from '@mocks';
+
 /* Templates */
 import { PdfCoursesTemplate } from '@domain/templates';
 
@@ -5,5 +8,12 @@ describe('Test in PdfCoursesTemplate', () => {
     it('should have respective methods', () => {
         expect(PdfCoursesTemplate).toHaveProperty('generate');
         expect(typeof PdfCoursesTemplate.generate).toBe('function');
+    });
+
+    it('should generate a pdf template string', () => {
+        const template = PdfCoursesTemplate.generate({ fullName: 'John Doe', courses: coursesWithLessonsMock });
+
+        expect(typeof template).toBe('string');
+        expect(template).toContain('John Doe');
     });
 });
