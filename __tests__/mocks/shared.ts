40db16aa2086308ba96813ab9129db5e09765c42
KristhDev <kristhdev@gmail.com>
2024-11-19 10:55:52 -0600

    Test(mocks): add more mocks

--- __tests__/mocks/shared.ts
+++ __tests__/mocks/shared.ts
@@ -4,12 +4,14 @@ import * as useImage from '@shared/hooks/useImage';
 import * as useNetwork from '@shared/hooks/useNetwork';
 import * as usePermissions from '@shared/hooks/usePermissions';
 import * as useStatus from '@shared/hooks/useStatus';
+import * as useVoiceRecorder from '@shared/hooks/useVoiceRecorder';
 
 export const useEmailSpy = jest.spyOn(useEmail, 'default');
 export const useImageSpy = jest.spyOn(useImage, 'default');
 export const useNetworkSpy = jest.spyOn(useNetwork, 'default');
 export const usePermissionsSpy = jest.spyOn(usePermissions, 'default');
 export const useStatusSpy = jest.spyOn(useStatus, 'default');
+export const useVoiceRecorderSpy = jest.spyOn(useVoiceRecorder, 'default');
 
 export const onCancelMock = jest.fn();
 export const onChangeValueMock = jest.fn();
@@ -18,7 +20,12 @@ export const onCloseMock = jest.fn();
 export const onCofirmMock = jest.fn();
 export const onErrorMock = jest.fn();
 export const onFinishMock = jest.fn();
+export const onNotRecordingMock = jest.fn();
 export const onPressMock = jest.fn();
+export const onRecordMock = jest.fn();
 export const onSearchMock = jest.fn();
 export const onSuccessMock = jest.fn();
 export const onToggleMock = jest.fn();
+
+export const startRecordingMock = jest.fn();
+export const stopRecordingMock = jest.fn();
