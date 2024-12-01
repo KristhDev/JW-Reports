481c9cb2f800eb6977aaa5d43172bf71039f0bfa
KristhDev <kristhdev@gmail.com>
2024-11-30 20:36:12 -0600

    Refactor(config): fix use of envs

--- src/config/supabase.ts
+++ src/config/supabase.ts
@@ -4,15 +4,12 @@ import { createClient } from '@supabase/supabase-js';
 /* Adapters */
 import { storage, storageKeys } from '@infrasturcture/adapters';
 
-/* Env */
-import { SUPABASE_APY_KEY, SUPABASE_URL } from '@env';
-
 import 'react-native-url-polyfill/auto';
 
 /* Creating a client that will be used to connect to the database. */
 export const supabase = createClient(
-    SUPABASE_URL,
-    SUPABASE_APY_KEY,
+    process.env.EXPO_PUBLIC_SUPABASE_URL!,
+    process.env.EXPO_PUBLIC_SUPABASE_APY_KEY!,
     {
         auth: {
             autoRefreshToken: true,
