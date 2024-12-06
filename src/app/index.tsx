1f01ae3bfc747901a7f95af1e3cfed6526f31146
KristhDev <kristhdev@gmail.com>
2024-12-06 17:41:19 -0600

    Refactor(app): remove redirect in root screen

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Redirect } from 'expo-router';
 
-import { useAuth } from '@auth';
-
-export default function MainScreen() {
-    const { state: { isAuthenticated } } = useAuth();
-
-    if (isAuthenticated) return <Redirect href="/(app)/(tabs)/preaching" />;
-    else return <Redirect href="/auth/login" />;
+export default function RootScreen() {
+    return <Redirect href="/auth/login" />;
 }
