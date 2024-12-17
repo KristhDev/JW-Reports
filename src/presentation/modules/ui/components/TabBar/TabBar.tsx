4af0c26bf939559540bed20fa9b53eb4743e3aec
KristhDev <kristhdev@gmail.com>
2024-12-16 21:40:14 -0600

    Refactor(ui): fix logic of navigation in TabBar

--- src/presentation/modules/ui/components/TabBar/TabBar.tsx
+++ src/presentation/modules/ui/components/TabBar/TabBar.tsx
@@ -1,6 +1,5 @@
 import React, { FC } from 'react';
 import { View } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 import { BottomTabBarProps } from '@react-navigation/bottom-tabs';
 import { useStyles } from 'react-native-unistyles';
 
@@ -13,6 +12,13 @@ import { useUI } from '../../hooks';
 /* Styles */
 import { stylesheet } from './styles';
 
+export interface NavigateOptions {
+    key: string;
+    index: number;
+    routeName: string;
+    routeParams?: any;
+}
+
 /**
  * This component is responsible for displaying a custom navigation bar
  * for the app
@@ -21,18 +27,12 @@ import { stylesheet } from './styles';
  * props for functionality of the component
  * @return {JSX.Element} Return jsx element to render tab bar of navigation
  */
-export const TabBar: FC<BottomTabBarProps> = ({ state, descriptors }): JSX.Element => {
-    const navigation = useNavigation();
+export const TabBar: FC<BottomTabBarProps> = ({ state, descriptors, navigation }): JSX.Element => {
     const { styles } = useStyles(stylesheet);
 
     const { state: { keyboard } } = useUI();
 
     const icons = [ 'home-outline', 'briefcase-outline', 'book-outline' ];
-    const firstScreens = {
-        'PreachingStackNavigation': 'HomeScreen',
-        'RevisitsStackNavigation': 'RevisitsTopTabsNavigation',
-        'CoursesStackNavigation': 'CoursesTopTabsNavigation',
-    }
 
     /**
      * This function returns the tint color of the icon in the tab bar.
@@ -48,6 +48,35 @@ export const TabBar: FC<BottomTabBarProps> = ({ state, descriptors }): JSX.Eleme
             : descriptors[state.routes[index].key]?.options.tabBarInactiveTintColor;
     }
 
+    /**
+     * This function is called when a tab bar button is pressed. It emits a
+     * 'tabPress' event, and if the event is not prevented, it navigates to the
+     * route specified by the routeName and routeParams if they are not
+     * undefined.
+     *
+     * @param {{ key: string, index: number, routeName: string, routeParams?: any }} options
+     * - The options object that is passed to the function.
+     * @param {string} options.key - The key of the route that the tab bar button
+     * is associated with.
+     * @param {number} options.index - The index of the route in the state routes
+     * array.
+     * @param {string} options.routeName - The name of the route to navigate to.
+     * @param {any} [options.routeParams] - The route params to pass to the
+     * navigate function.
+     * @return {void} The function does not return anything.
+     */
+    const handleNavigate = ({ key, index, routeName, routeParams }: NavigateOptions): void => {
+        const event = navigation.emit({
+            type: 'tabPress',
+            target: key,
+            canPreventDefault: true
+        });
+
+        if (!event.defaultPrevented && index !== state.index) {
+            navigation.navigate(routeName, routeParams);
+        }
+    }
+
     if (keyboard.isVisible) return (<></>);
 
     return (
@@ -58,7 +87,7 @@ export const TabBar: FC<BottomTabBarProps> = ({ state, descriptors }): JSX.Eleme
                     color={ handleGetTintColor(index) }
                     iconName={ icons[index] }
                     key={ route.key }
-                    onPress={ () => navigation.navigate({ name: route.name, params: { screen: firstScreens[(route.name as keyof typeof firstScreens)] } } as never) }
+                    onPress={ () => handleNavigate({ key: route.key, index, routeName: route.name }) }
                     title={ descriptors[route.key]?.options.title || '' }
                     totalTabs={ state.routes.length }
                 />
