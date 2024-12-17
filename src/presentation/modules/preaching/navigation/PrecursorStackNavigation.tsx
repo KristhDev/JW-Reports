1e685ab45ed23775358d4cd20d78ade8b142a079
KristhDev <kristhdev@gmail.com>
2024-12-16 21:41:43 -0600

    Refactor(preaching): add more navigations to split precursors and publisher logic

---
+++ src/presentation/modules/preaching/navigation/PrecursorStackNavigation.tsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { useStyles } from 'react-native-unistyles';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+
+/* Screens */
+import { AddOrEditPreaching, PrecursorHome } from '../screens';
+
+/* Components */
+import { BackButton, HeaderButtons } from '@ui';
+
+/* Hooks */
+import { useAuth } from '@auth';
+import { useNetwork, useStatus } from '@shared';
+import { usePreaching } from '../hooks';
+
+/* Interfaces */
+import { PrecursorStackParamsList } from '../interfaces';
+
+const Stack = createStackNavigator<PrecursorStackParamsList>();
+
+/**
+ * This is a stack navigation for preaching.
+ *
+ * @return {JSX.Element} rendered component to show stack navigation of preaching
+ */
+const PrecursorStackNavigation = (): JSX.Element => {
+    const [ showDeletePreachingModal, setShowDeletePreachingModal ] = useState<boolean>(false);
+
+    const { state: { user } } = useAuth();
+
+    const {
+        state: {
+            isPreachingDeleting,
+            seletedPreaching,
+            selectedDate
+        },
+        deletePreaching,
+        setSelectedDate,
+        loadPreachings
+    } = usePreaching();
+
+    const { theme: { colors, margins } } = useStyles();
+    const { setNetworkError } = useStatus();
+    const { wifi } = useNetwork();
+
+    /**
+     * If the user clicks the delete button, then show the delete modal, and if the user clicks the
+     * confirm button, then delete the preaching.
+     *
+     * @return {void} This function does not return anything
+     */
+    const handleDeleteConfirm = (): void => {
+        deletePreaching(() => setShowDeletePreachingModal(false));
+    }
+
+    /**
+     * Effect to select default date and load courses.
+     */
+    useEffect(() => {
+        setSelectedDate(new Date());
+    }, []);
+
+    /**
+     * Effect to load preachings of the selected date.
+     */
+    useEffect(() => {
+        if (!wifi.hasConnection) {
+            setNetworkError();
+            return;
+        }
+
+        if (user.id === '') return;
+        loadPreachings(selectedDate);
+    } ,[ selectedDate ]);
+
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                cardStyle: {
+                    backgroundColor: colors.background,
+                },
+                headerStyle: {
+                    backgroundColor: colors.header
+                },
+                headerTintColor: colors.headerText,
+                headerShadowVisible: false
+            }}
+        >
+            <Stack.Screen
+                component={ PrecursorHome }
+                name="HomeScreen"
+                options={{
+                    headerRight: () => (
+                        <HeaderButtons
+                            logoutButton
+                            changeMonthButton
+                            settingsButtons
+                        />
+                    ),
+                    title: 'Inicio'
+                }}
+            />
+
+            <Stack.Screen
+                component={ AddOrEditPreaching }
+                name="AddOrEditPreachingScreen"
+                options={{
+                    headerTitleStyle: { marginLeft: -margins.xs },
+                    headerLeft: ({ onPress }) => <BackButton onPress={ onPress } />,
+                    headerRight: () => (
+                        <HeaderButtons
+                            deleteButton={ seletedPreaching.id !== '' }
+                            deleteModalText="¿Está seguro de eliminar este día de predicación?"
+                            isDeleteModalLoading={ isPreachingDeleting }
+                            onCloseDeleteModal={ () => setShowDeletePreachingModal(false) }
+                            onConfirmDeleteModal={ handleDeleteConfirm }
+                            onShowDeleteModal={ () => setShowDeletePreachingModal(true) }
+                            showDeleteModal={ showDeletePreachingModal }
+                        />
+                    ),
+                    title: `${ seletedPreaching.id !== '' ? 'Editar' : 'Agregar' } predicación`
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+export default PrecursorStackNavigation;
---
+++ src/presentation/modules/preaching/navigation/PublisherStackNavigation.tsx
@@ -0,0 +1,186 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { CardStyleInterpolators, createStackNavigator } from '@react-navigation/stack';
+import { useStyles } from 'react-native-unistyles';
+
+import { useCourses } from '@courses';
+import { AddOrEditLesson, LessonDetail, useLessons } from '@lessons';
+import { AddOrEditRevisit, RevisitDetail, useRevisits } from '@revisits';
+import { PublisherHome, PublisherStackParamsList } from '@preaching';
+import { useNetwork, useStatus } from '@shared';
+import { BackButton, HeaderButtons } from '@ui';
+
+import { Characters } from '@utils';
+
+const Stack = createStackNavigator<PublisherStackParamsList>();
+
+const PublisherStackNavigation = (): JSX.Element => {
+    const [ showDeleteLessonModal, setShowDeleteLessonModal ] = useState<boolean>(false);
+    const [ showDeleteRevisitModal, setShowDeleteRevisitModal ] = useState<boolean>(false);
+
+    const navigation = useNavigation();
+    const { theme: { colors, margins } } = useStyles();
+
+    const { state: { selectedCourse } } = useCourses();
+    const { state: { isRevisitDeleting, selectedRevisit }, deleteRevisit, loadLastRevisit } = useRevisits();
+    const { state: { isLessonDeleting, selectedLesson }, deleteLesson, loadLastLesson } = useLessons();
+    const { setNetworkError } = useStatus();
+    const { wifi } = useNetwork();
+
+    const revisitDetailTitle = `Revisita ${ selectedRevisit.personName }`;
+
+    /**
+     * When the user clicks the delete button, show the delete modal, and when the user clicks the
+     * delete button in the modal, delete the lesson.
+     *
+     * @return {void} This function does not return anything
+     */
+    const handleDeleteLesson = (): void => {
+        deleteLesson(true, () => setShowDeleteLessonModal(false));
+    }
+
+    /**
+     * If the user clicks the delete button, then show the delete modal, and if the user clicks the
+     * confirm button, then delete the preaching.
+     *
+     * @return {void} This function does not return anything
+     */
+    const handleDeleteRevisit = (): void => {
+        deleteRevisit(true, () => setShowDeleteRevisitModal(false));
+    }
+
+    useEffect(() => {
+        if (!wifi.hasConnection) {
+            setNetworkError();
+            return;
+        }
+
+        loadLastLesson();
+        loadLastRevisit();
+    } ,[]);
+
+    return (
+        <Stack.Navigator
+            screenOptions={{
+                cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+                cardStyle: {
+                    backgroundColor: colors.background,
+                },
+                headerStyle: {
+                    backgroundColor: colors.header
+                },
+                headerTintColor: colors.headerText,
+                headerShadowVisible: false
+            }}
+        >
+            <Stack.Screen
+                component={ PublisherHome }
+                name="HomeScreen"
+                options={{
+                    headerRight: () => (
+                        <HeaderButtons
+                            logoutButton
+                            changeMonthButton
+                            settingsButtons
+                        />
+                    ),
+                    title: 'Inicio'
+                }}
+            />
+
+            <Stack.Screen
+                component={ AddOrEditLesson }
+                name="AddOrEditLessonScreen"
+                options={{
+                    headerTitleStyle: { marginLeft: -margins.xs },
+                    headerLeft: ({ onPress }) => <BackButton onPress={ onPress } />,
+                    headerRight: () => (
+                        <HeaderButtons
+                            deleteButton={ selectedLesson.id !== '' }
+                            deleteModalText="¿Está seguro de eliminar esta clase?"
+                            isDeleteModalLoading={ isLessonDeleting }
+                            onCloseDeleteModal={ () => setShowDeleteLessonModal(false) }
+                            onConfirmDeleteModal={ handleDeleteLesson }
+                            onShowDeleteModal={ () => setShowDeleteLessonModal(true) }
+                            showDeleteModal={ showDeleteLessonModal }
+                        />
+                    ),
+                    title: `${ selectedLesson.id !== '' ? 'Editar' : 'Agregar' } clase`
+                }}
+            />
+
+            <Stack.Screen
+                component={ LessonDetail }
+                name="LessonDetailScreen"
+                options={{
+                    headerTitleStyle: { marginLeft: -margins.xs },
+                    headerLeft: ({ onPress }) => <BackButton onPress={ onPress } />,
+                    headerRight: () => (
+                        <HeaderButtons
+                            deleteButton={ true }
+                            deleteModalText="¿Está seguro de eliminar esta clase?"
+                            isDeleteModalLoading={ isLessonDeleting }
+                            onCloseDeleteModal={ () => setShowDeleteLessonModal(false) }
+                            onConfirmDeleteModal={ handleDeleteLesson }
+                            onShowDeleteModal={ () => setShowDeleteLessonModal(true) }
+                            showDeleteModal={ showDeleteLessonModal }
+
+                            editButton={ !selectedCourse.finished || !selectedCourse.suspended }
+                            onPressEditButton={ () => navigation.navigate({ name: 'PublisherStackNavigation', params: { screen: 'AddOrEditLessonScreen' } } as never) }
+                        />
+                    ),
+                    title: `Clase con ${ selectedCourse.personName }`
+                }}
+            />
+
+            <Stack.Screen
+                component={ AddOrEditRevisit }
+                name="AddOrEditRevisitScreen"
+                options={{
+                    headerTitleStyle: { marginLeft: -margins.xs },
+                    headerLeft: ({ onPress }) => <BackButton onPress={ onPress } />,
+                    headerRight: () => (
+                        <HeaderButtons
+                            deleteButton={ selectedRevisit.id !== '' }
+                            deleteModalText="¿Está seguro de eliminar esta revisita?"
+                            isDeleteModalLoading={ isRevisitDeleting }
+                            onCloseDeleteModal={ () => setShowDeleteRevisitModal(false) }
+                            onConfirmDeleteModal={ handleDeleteRevisit }
+                            onShowDeleteModal={ () => setShowDeleteRevisitModal(true) }
+                            showDeleteModal={ showDeleteRevisitModal }
+
+                            editButton={ false }
+                        />
+                    ),
+                    title: `${ selectedRevisit.id !== '' ? 'Editar' : 'Agregar' } revisita`
+                }}
+            />
+
+            <Stack.Screen
+                component={ RevisitDetail }
+                name="RevisitDetailScreen"
+                options={{
+                    headerTitleStyle: { marginLeft: -margins.xs },
+                    headerLeft: ({ onPress }) => <BackButton onPress={ onPress } />,
+                    headerRight: () => (
+                        <HeaderButtons
+                            deleteButton={ true }
+                            deleteModalText="¿Está seguro de eliminar esta revisita?"
+                            isDeleteModalLoading={ isRevisitDeleting }
+                            onCloseDeleteModal={ () => setShowDeleteRevisitModal(false) }
+                            onConfirmDeleteModal={ handleDeleteRevisit }
+                            onShowDeleteModal={ () => setShowDeleteRevisitModal(true) }
+                            showDeleteModal={ showDeleteRevisitModal }
+
+                            editButton={ true }
+                            onPressEditButton={ () => navigation.navigate({ name: 'PublisherStackNavigation', params: { screen: 'AddOrEditRevisitScreen' } } as never) }
+                        />
+                    ),
+                    title: Characters.truncate(revisitDetailTitle, 22)
+                }}
+            />
+        </Stack.Navigator>
+    );
+}
+
+export default PublisherStackNavigation;
