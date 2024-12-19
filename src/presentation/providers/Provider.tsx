4845b9f1597c25b92e2c070d59e281dd9f437794
KristhDev <kristhdev@gmail.com>
2024-12-19 12:37:56 -0600

    Feat(providers): add UnistylesProvider in global Provider

--- src/presentation/providers/Provider.tsx
+++ src/presentation/providers/Provider.tsx
@@ -1,5 +1,6 @@
 import React, { FC, PropsWithChildren } from 'react';
 import { Provider as ReduxProvider } from 'react-redux';
+import { UnistylesProvider } from 'react-native-unistyles';
 import { KeyboardProvider } from 'react-native-keyboard-controller';
 import { PersistGate } from 'reduxjs-toolkit-persist/lib/integration/react';
 import { MenuProvider } from 'react-native-popup-menu';
@@ -20,17 +21,19 @@ import { NetworkProvider, ThemeProvider } from '@application/context';
 const Provider: FC<PropsWithChildren> = ({ children }): JSX.Element => {
     return (
         <KeyboardProvider>
-            <NetworkProvider>
-                <MenuProvider>
-                    <ThemeProvider>
-                        <ReduxProvider store={ store }>
-                            <PersistGate loading={ null } persistor={ persistor }>
-                                { children }
-                            </PersistGate>
-                        </ReduxProvider>
-                    </ThemeProvider>
-                </MenuProvider>
-            </NetworkProvider>
+            <UnistylesProvider>
+                <NetworkProvider>
+                    <MenuProvider>
+                        <ThemeProvider>
+                            <ReduxProvider store={ store }>
+                                <PersistGate loading={ null } persistor={ persistor }>
+                                    { children }
+                                </PersistGate>
+                            </ReduxProvider>
+                        </ThemeProvider>
+                    </MenuProvider>
+                </NetworkProvider>
+            </UnistylesProvider>
         </KeyboardProvider>
     );
 }
