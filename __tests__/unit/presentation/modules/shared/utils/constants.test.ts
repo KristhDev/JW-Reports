dfa6bc6386434fd736dd240b926d11b3da3d48aa
KristhDev <kristhdev@gmail.com>
2025-01-11 15:17:23 -0600

    Refactor(tests): remove unused permissionsStatus test from constants util

--- __tests__/unit/presentation/modules/shared/utils/constants.test.ts
+++ __tests__/unit/presentation/modules/shared/utils/constants.test.ts
@@ -1,11 +1,7 @@
-import { permissionsStatus, reportErrorDefaultImgs } from '@shared';
+import { reportErrorDefaultImgs } from '@shared';
 
 describe('Test in constants util', () => {
     it('should to match snapshot - reportErrorDefaultImgs', () => {
         expect(reportErrorDefaultImgs).toMatchSnapshot();
-    });
-
-    it('should to match snapshot - permissionsStatus', () => {
-        expect(permissionsStatus).toMatchSnapshot();
     });
 });
