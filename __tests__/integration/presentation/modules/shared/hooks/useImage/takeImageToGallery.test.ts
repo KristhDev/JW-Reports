3596493afb08c582820f251e5bd160f88c0746d0
KristhDev <kristhdev@gmail.com>
2025-01-14 10:05:01 -0600

    Test(integration): enhance takeImageToGallery tests for blocked and unavailable permissions

--- __tests__/integration/presentation/modules/shared/hooks/useImage/takeImageToGallery.test.ts
+++ __tests__/integration/presentation/modules/shared/hooks/useImage/takeImageToGallery.test.ts
@@ -1,18 +1,17 @@
 import { act } from '@testing-library/react-native';
-import { request } from 'react-native-permissions';
 
 /* Setups */
 import { getMockStoreUseImage, renderUseImage } from '@setups';
 
 /* Mocks */
 import {
+    blockedStateMock,
     deniedStateMock,
     DeviceImageServiceSpy,
-    DeviceInfoSpy,
     grantedStateMock,
     imageModelMock,
     initialStatusStateMock,
-    undeterminedStateMock
+    unavailableStateMock,
 } from '@mocks';
 
 /* Constants */
@@ -25,7 +24,6 @@ describe('Test in useImage hook - takeImageToGallery', () => {
 
     it('should take image to gallery', async () => {
         DeviceImageServiceSpy.openPicker.mockResolvedValueOnce(imageModelMock);
-        DeviceInfoSpy.getSystemVersion.mockImplementation(() => '12');
 
         const mockStore = getMockStoreUseImage({ permissions: grantedStateMock, status: initialStatusStateMock });
         const { result } = renderUseImage(mockStore);
@@ -39,37 +37,9 @@ describe('Test in useImage hook - takeImageToGallery', () => {
         expect(result.current.useImage.image).toEqual(imageModelMock);
     });
 
-    // it('should not access to gallery if permission is blocked in android below 13', async () => {
-    //     DeviceInfoSpy.getSystemVersion.mockImplementation(() => '12');
-
-    //     const mockStore = getMockStoreUseImage({
-    //         permissions: {
-    //             ...blockedStateMock,
-    //             permissions: { ...blockedStateMock.permissions, mediaLibrary: 'undetermined' }
-    //         },
-    //         status: initialStatusStateMock
-    //     });
-
-    //     const { result } = renderUseImage(mockStore);
-
-    //     await act(async () => {
-    //         await result.current.useImage.takeImageToGallery();
-    //     });
-
-    //     /* Check if openPicker isnt called and image is empty */
-    //     expect(DeviceImageServiceSpy.openPicker).not.toHaveBeenCalled();
-    //     expect(result.current.useImage.image).toBeNull();
-
-    //     expect(result.current.useStatus.state).toEqual({
-    //         msg: permissionsMessages.REQUEST,
-    //         code: 401
-    //     });
-    // });
-
     it('should request permissions if permission is denied', async () => {
         DeviceImageServiceSpy.openPicker.mockResolvedValueOnce(imageModelMock);
-        (request as jest.Mock).mockResolvedValue(permissionsStatus.GRANTED);
-        DeviceInfoSpy.getSystemVersion.mockImplementation(() => '12');
+        DeviceImageServiceSpy.requestMediaLibraryPermission.mockResolvedValueOnce(permissionsStatus.GRANTED);
 
         const mockStore = getMockStoreUseImage({
             permissions: {
@@ -86,15 +56,34 @@ describe('Test in useImage hook - takeImageToGallery', () => {
         });
 
         /* Check if DeviceImageServiceSpy.openPicker is called one time and image is equal to mock */
-        expect(request).toHaveBeenCalledTimes(1);
         expect(DeviceImageServiceSpy.openPicker).toHaveBeenCalledTimes(1);
         expect(result.current.useImage.image).toEqual(imageModelMock);
     });
 
-    it('should not access to gallery if permission is undermined', async () => {
-        DeviceInfoSpy.getSystemVersion.mockImplementation(() => '12');
+    it('should not access to gallery if permission is blocked', async () => {
+        const mockStore = getMockStoreUseImage({
+            permissions: blockedStateMock,
+            status: initialStatusStateMock
+        });
 
-        const mockStore = getMockStoreUseImage({ permissions: undeterminedStateMock, status: initialStatusStateMock });
+        const { result } = renderUseImage(mockStore);
+
+        await act(async () => {
+            await result.current.useImage.takeImageToGallery();
+        });
+
+        /* Check if openPicker isnt called and image is empty */
+        expect(DeviceImageServiceSpy.openPicker).not.toHaveBeenCalled();
+        expect(result.current.useImage.image).toBeNull();
+
+        expect(result.current.useStatus.state).toEqual({
+            msg: permissionsMessages.REQUEST,
+            code: 401
+        });
+    });
+
+    it('should not access to gallery if permission is unavailable', async () => {
+        const mockStore = getMockStoreUseImage({ permissions: unavailableStateMock, status: initialStatusStateMock });
         const { result } = renderUseImage(mockStore);
 
         await act(async () => {
