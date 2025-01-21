d73e50b83ce4be3c690ad9c58c9c97d9217e4250
KristhDev <kristhdev@gmail.com>
2025-01-21 07:28:25 -0600

    Test(unit): add unit tests for permissionDenied method in ExternalStorageError

--- __tests__/unit/domain/errors/external-storage.error.test.ts
+++ __tests__/unit/domain/errors/external-storage.error.test.ts
@@ -1,10 +1,27 @@
+/* Constants */
+import { permissionsMessages } from '@application/constants';
+
+/* Errors */
 import { ExternalStorageError } from '@domain/errors';
 
 describe('Test in ExternalStorageError', () => {
     it('should have respective methods and properties', () => {
+        expect(ExternalStorageError).toHaveProperty('permissionDenied');
+        expect(ExternalStorageError.permissionDenied).toBeInstanceOf(Function);
+
         const error = new ExternalStorageError('File not found');
 
         expect(error.message).toBe('File not found');
+        expect(error.name).toBe('ExternalStorageError');
+
+        expect(error).toBeInstanceOf(Error);
+        expect(error).toBeInstanceOf(ExternalStorageError);
+    });
+
+    it('should create a new instance of ExternalStorageError with permissionDenied', () => {
+        const error = ExternalStorageError.permissionDenied(permissionsMessages.FILE_EXPORT_DENIED);
+
+        expect(error.message).toBe(permissionsMessages.FILE_EXPORT_DENIED);
         expect(error.name).toBe('ExternalStorageError');
 
         expect(error).toBeInstanceOf(Error);
