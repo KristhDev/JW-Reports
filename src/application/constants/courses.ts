07529745a213ad36853ca6183ee7882652ab2e11
KristhDev <kristhdev@gmail.com>
2025-03-25 18:05:46 -0600

    Refactor(constants): integrate translations for course placeholders and validation messages

--- src/application/constants/courses.ts
+++ src/application/constants/courses.ts
@@ -1,13 +1,75 @@
+import { TranslationAdapter } from '@infrastructure/adapters/translation.adapter';
+
+export const coursesPlaceholders = {
+    PERSON_NAME: TranslationAdapter.translate('forms.placeholders.typeThe', {
+        attribute: TranslationAdapter.translate('forms.fields.name'),
+        article: 'el'
+    }),
+
+    PERSON_ADDRESS: TranslationAdapter.translate('forms.placeholders.typeThe', {
+        attribute: TranslationAdapter.translate('forms.fields.address'),
+        article: 'la'
+    }),
+
+    PERSON_ABOUT: TranslationAdapter.translate('forms.placeholders.personAbout'),
+
+    PUBLICATION: TranslationAdapter.translate('forms.placeholders.typeThe', {
+        attribute: TranslationAdapter.translate('forms.fields.publication'),
+        article: 'la'
+    })
+}
+
 export const coursesMessages = {
     EXPORTED_SUCCESS: 'Has exportado tus cursos correctamente. El archivo se encuentra en la carpeta que ha seleccionado.',
-    PERSON_NAME_REQUIRED: 'El nombre de la persona es requerido.',
-    PERSON_NAME_MIN_LENGTH: 'El nombre de la persona debe tener al menos 2 caracteres.',
-    PERSON_ABOUT_REQUIRED: 'La información de la persona es requerida.',
-    PERSON_ABOUT_MIN_LENGTH: 'La información de la persona debe tener al menos 10 caracteres.',
-    PERSON_ADDRESS_REQUIRED: 'La dirección es requerida.',
-    PERSON_ADDRESS_MIN_LENGTH: 'La dirección debe tener al menos 10 caracteres.',
-    PUBLICATION_REQUIRED: 'La publicación de estudio no puede estar vacía.',
-    PUBLICATION_MIN_LENGTH: 'La publicación de estudio debe tener al menos 5 caracteres.',
+
+    PERSON_NAME_REQUIRED: TranslationAdapter.translate('forms.validations.required', {
+        article: 'El',
+        attribute: TranslationAdapter.translate('forms.fields.personName'),
+        required: 'requerido'
+    }),
+
+    PERSON_NAME_MIN_LENGTH: TranslationAdapter.translate('forms.validations.min', {
+        article: 'El',
+        attribute: TranslationAdapter.translate('forms.fields.personName'),
+        min: 2
+    }),
+
+    PERSON_ABOUT_REQUIRED: TranslationAdapter.translate('forms.validations.required', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.personAbout'),
+        required: 'requerida'
+    }),
+
+    PERSON_ABOUT_MIN_LENGTH: TranslationAdapter.translate('forms.validations.min', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.personAbout'),
+        min: 10
+    }),
+
+    PERSON_ADDRESS_REQUIRED: TranslationAdapter.translate('forms.validations.required', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.address'),
+        required: 'requerida'
+    }),
+
+    PERSON_ADDRESS_MIN_LENGTH: TranslationAdapter.translate('forms.validations.min', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.address'),
+        min: 10
+    }),
+
+    PUBLICATION_REQUIRED: TranslationAdapter.translate('forms.validations.empty', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.publication'),
+        empty: 'vacía'
+    }),
+
+    PUBLICATION_MIN_LENGTH: TranslationAdapter.translate('forms.validations.min', {
+        article: 'La',
+        attribute: TranslationAdapter.translate('forms.fields.publication'),
+        min: 5
+    }),
+
     UNSELECTED: 'No hay un curso seleccionado.',
     UNSELECTED_FINISH_OR_START: 'No pudes terminar o comenzar de nuevo un curso suspendido.',
     UNSELECTED_DELETE: 'No hay un curso seleccionado para eliminar.',
