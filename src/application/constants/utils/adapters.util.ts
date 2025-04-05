f7eda4e5e61088b3e7fd4314a498b22b035a3a7a
KristhDev <kristhdev@gmail.com>
2025-04-04 22:46:38 -0600

    Feat(constants): add language codes and valid language codes for localization support

--- src/application/constants/utils/adapters.util.ts
+++ src/application/constants/utils/adapters.util.ts
@@ -1,5 +1,7 @@
 import { EncodingType } from 'expo-file-system';
 
+import { Languages } from '@infrastructure/interfaces';
+
 export const fileEncodings = {
     BASE64: EncodingType.Base64,
     UTF8: EncodingType.UTF8
@@ -12,3 +14,13 @@ export const storageKeys = {
     STORE: 'jwreports-store',
     THEME: 'jwreports-theme'
 }
+
+export const languagesCodes: Record<Uppercase<Languages>, Languages> = {
+    EN: 'en',
+    ES: 'es'
+}
+
+export const validLanguagesCodes: Languages[] = [
+    'en',
+    'es'
+];
