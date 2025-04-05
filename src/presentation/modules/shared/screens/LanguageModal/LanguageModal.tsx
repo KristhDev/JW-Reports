7d2b749bfd949651bfeaff0cf83b3901822b07ba
KristhDev <kristhdev@gmail.com>
2025-04-04 22:45:32 -0600

    Feat(shared): add LanguageModal component for language selection

---
+++ src/presentation/modules/shared/screens/LanguageModal/LanguageModal.tsx
@@ -0,0 +1,38 @@
+import React, { FC } from 'react';
+
+/* Constants */
+import { LANGUAGE_OPTIONS } from '@application/constants/utils';
+
+/* Screens */
+import { OptionsModal } from '@ui/screens';
+
+/* Hooks */
+import { useTranslation, useUI } from '@ui/hooks';
+
+/* Interfaces */
+import { ModalProps } from '@ui/interfaces';
+import { Languages } from '@infrastructure/interfaces';
+
+/**
+ * This is a modal that allows the user to change the theme.
+ *
+ * @param {ModalProps} { isOpen, onClose }
+ * @return {JSX.Element} Return jsx element to render modal of theme
+ */
+const LanguageModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element => {
+    const { changeLanguage } = useTranslation();
+    const { state: { userInterface } } = useUI();
+
+    return (
+        <OptionsModal
+            isOpen={ isOpen }
+            items={ LANGUAGE_OPTIONS }
+            onCancel={ onClose }
+            onChangeValue={ (value) => changeLanguage(value as Languages) }
+            title="Idioma"
+            value={ userInterface.language }
+        />
+    );
+}
+
+export default LanguageModal;
--- src/presentation/modules/shared/screens/index.ts
+++ src/presentation/modules/shared/screens/index.ts
@@ -1,5 +1,6 @@
 export * from './ExportData';
 export * from './Feedback';
+export * from './LanguageModal';
 export * from './ReportError';
 export * from './Settings';
 export * from './StatusModal';
