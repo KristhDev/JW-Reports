9301193bef55e0f68f02ee9754bfa7251eff08bc
KristhDev <kristhdev@gmail.com>
2025-04-07 12:16:49 -0600

    Feat(contracts): add MINISTRY_PARTICIPATIONS getter to PublisherServiceContract

--- src/domain/contracts/services/publisher.service.contract.ts
+++ src/domain/contracts/services/publisher.service.contract.ts
@@ -1,5 +1,6 @@
-import { ItemOption } from '@infrastructure/interfaces';
+import { ItemOption, ParticipateInMinistryItem } from '@infrastructure/interfaces';
 
 export abstract class PublisherServiceContract {
+    public abstract get MINISTRY_PARTICIPATIONS(): ParticipateInMinistryItem[];
     public abstract get PRECURSORS_OPTIONS(): ItemOption[];
 }
