4103d341032cb54484b7c5d21179e699f12cd036
KristhDev <kristhdev@gmail.com>
2025-04-08 11:41:49 -0600

    Feat(contracts): add TABLE_PREACHING_HEADERS abstract getter to PublisherServiceContract

--- src/domain/contracts/services/publisher.service.contract.ts
+++ src/domain/contracts/services/publisher.service.contract.ts
@@ -3,4 +3,5 @@ import { ItemOption, ParticipateInMinistryItem } from '@infrastructure/interface
 export abstract class PublisherServiceContract {
     public abstract get MINISTRY_PARTICIPATIONS(): ParticipateInMinistryItem[];
     public abstract get PRECURSORS_OPTIONS(): ItemOption[];
+    public abstract get TABLE_PREACHING_HEADERS(): string[];
 }
