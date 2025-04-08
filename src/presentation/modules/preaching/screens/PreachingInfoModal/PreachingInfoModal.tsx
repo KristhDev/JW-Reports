3dbbb589f67c400066af10f29498ae9cc37eedf0
KristhDev <kristhdev@gmail.com>
2025-04-08 11:42:22 -0600

    Feat(preaching): integrate translation for modal text in PreachingInfoModal

--- src/presentation/modules/preaching/screens/PreachingInfoModal/PreachingInfoModal.tsx
+++ src/presentation/modules/preaching/screens/PreachingInfoModal/PreachingInfoModal.tsx
@@ -19,6 +19,7 @@ import { Button } from '@ui/components';
 /* Hooks */
 import { useAuth } from '@auth/hooks';
 import { usePreaching } from '../../hooks';
+import { useTranslation } from '@ui/hooks';
 
 import { ModalProps } from '@ui/interfaces';
 
@@ -41,6 +42,7 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
 
     const { state: { user } } = useAuth();
     const { state: { preachings } } = usePreaching();
+    const { translate } = useTranslation();
 
     const preachingsOfWeek = TimeAdapter.getArrayValuesOfWeek<PreachingEntity>(preachings);
 
@@ -54,21 +56,34 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
         <Modal isOpen={ isOpen }>
             <View style={ themeStyles.modalContainer }>
                 <Text style={ styles.modalTitle }>
-                    Información de Predicación
+                    { translate('modals.preaching.titles.preachingInfo') }
                 </Text>
 
                 <View style={ styles.modalSection }>
-                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas aproximadas por semana:</Text>
-                    <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>{ hoursRequirementByWeek }</Text>
+                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                        { translate('modals.preaching.subTitles.hoursAproxPerWeek') }
+                    </Text>
+
+                    <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
+                        { hoursRequirementByWeek }
+                    </Text>
                 </View>
 
                 <View style={ styles.modalSection }>
-                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas completas:</Text>
-                    <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>{ hoursDoneByWeek }</Text>
+                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                        { translate('modals.preaching.subTitles.completedHours') }
+                    </Text>
+
+                    <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
+                        { hoursDoneByWeek }
+                    </Text>
                 </View>
 
                 <View style={ styles.modalSection }>
-                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas faltantes por semana:</Text>
+                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                        { translate('modals.preaching.subTitles.hoursMissingPerWeek') }
+                    </Text>
+
                     <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
                         { (remainingHoursOfWeeklyRequirement === '0:00' || isNegativeHWR)
                             ? preachingMessages.WEEKLY_HOURS_REQUIRED_DONE
@@ -79,7 +94,10 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
 
                 { (isNegativeHWR) && (
                     <View style={ styles.modalSection }>
-                        <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas extras hechas en está semana:</Text>
+                        <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                            { translate('modals.preaching.subTitles.overtimeWorkThisWeek') }
+                        </Text>
+
                         <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
                             { remainingHoursOfWeeklyRequirement }
                         </Text>
@@ -87,7 +105,10 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
                 ) }
 
                 <View style={ styles.modalSection }>
-                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas faltantes por mes:</Text>
+                    <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                        { translate('modals.preaching.subTitles.hoursMissingPerMonth') }
+                    </Text>
+
                     <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
                         { (reamainingOfHoursRequirement === '0:00' || isNegativeHR)
                             ? preachingMessages.MONTHLY_HOURS_REQUIRED_DONE
@@ -98,7 +119,10 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
 
                 { (isNegativeHR) && (
                     <View style={ styles.modalSection }>
-                        <Text style={{ ...styles.modalSectionText, color: colors.text }}>Horas extras hechas en este mes:</Text>
+                        <Text style={{ ...styles.modalSectionText, color: colors.text }}>
+                            { translate('modals.preaching.subTitles.overtimeWorkThisMonth') }
+                        </Text>
+
                         <Text style={{ ...styles.modalSectionText, color: colors.modalText }}>
                             { reamainingOfHoursRequirement }
                         </Text>
@@ -110,7 +134,7 @@ const PreachingInfoModal: FC<ModalProps> = ({ isOpen, onClose }): JSX.Element =>
                         containerStyle={{ paddingHorizontal: (margins.xs + 4), minWidth: 0 }}
                         onPress={ onClose }
                         pressableStyle={{ backgroundColor: 'transparent' }}
-                        text="ESTA BIEN"
+                        text={ translate('forms.actions.ok') }
                         textStyle={{ color: colors.button, fontSize: fontSizes.sm }}
                         underlayColor={ colors.buttonTranslucent }
                     />
