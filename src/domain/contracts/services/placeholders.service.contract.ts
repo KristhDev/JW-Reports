df047e38abe6dccda894ebfc5bc50dc2827eee13
KristhDev <kristhdev@gmail.com>
2025-04-10 08:49:58 -0600

    Feat(contracts): add preachingPlaceholders getter to PlaceholdersServiceContract

--- src/domain/contracts/services/placeholders.service.contract.ts
+++ src/domain/contracts/services/placeholders.service.contract.ts
@@ -1,6 +1,7 @@
-import { AuthPlaceholders, CoursesPlaceholders } from '@infrastructure/interfaces';
+import { AuthPlaceholders, CoursesPlaceholders, PreachingPlaceholders } from '@infrastructure/interfaces';
 
 export abstract class PlaceholdersServiceContract {
     public abstract get authPlaceholders(): AuthPlaceholders;
     public abstract get coursesPlaceholders(): CoursesPlaceholders;
+    public abstract get preachingPlaceholders(): PreachingPlaceholders
 }
