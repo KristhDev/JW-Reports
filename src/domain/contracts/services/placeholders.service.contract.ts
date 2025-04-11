9410ad72fd8a4772a35f9009664677e280b79343
KristhDev <kristhdev@gmail.com>
2025-04-10 21:49:20 -0600

    Feat(contracts): add lessonsPlaceholders to PlaceholdersServiceContract

--- src/domain/contracts/services/placeholders.service.contract.ts
+++ src/domain/contracts/services/placeholders.service.contract.ts
@@ -1,7 +1,8 @@
-import { AuthPlaceholders, CoursesPlaceholders, PreachingPlaceholders } from '@infrastructure/interfaces';
+import { AuthPlaceholders, CoursesPlaceholders, LessonsPlaceholders, PreachingPlaceholders } from '@infrastructure/interfaces';
 
 export abstract class PlaceholdersServiceContract {
     public abstract get authPlaceholders(): AuthPlaceholders;
     public abstract get coursesPlaceholders(): CoursesPlaceholders;
-    public abstract get preachingPlaceholders(): PreachingPlaceholders
+    public abstract get preachingPlaceholders(): PreachingPlaceholders;
+    public abstract get lessonsPlaceholders(): LessonsPlaceholders;
 }
