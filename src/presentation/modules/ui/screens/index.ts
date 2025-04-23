f1dba9ec62464a2d229e7f406e0b2771d88dc5eb
KristhDev <kristhdev@gmail.com>
2025-04-23 15:11:00 -0600

    Feat(ui): export LoadingScreen from index for improved module accessibility

--- src/presentation/modules/ui/screens/index.ts
+++ src/presentation/modules/ui/screens/index.ts
@@ -1,4 +1,5 @@
 export { DeleteModal } from './DeleteModal';
+export { LoadingScreen } from './LoadingScreen';
 export { Modal } from './Modal';
 export { MonthPickerModal } from './MonthPickerModal';
 export { OptionsModal } from './OptionsModal';
