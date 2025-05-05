cb2874f2ab2ac5a674b0e73a852c26e17e7f6f05
KristhDev <kristhdev@gmail.com>
2025-05-05 13:08:16 -0600

    Feat(contracts): add revisits placeholders to PlaceholdersServiceContract

--- src/domain/contracts/services/placeholders.service.contract.ts
+++ src/domain/contracts/services/placeholders.service.contract.ts
@@ -1,8 +1,9 @@
-import { AuthPlaceholders, CoursesPlaceholders, LessonsPlaceholders, PreachingPlaceholders } from '@infrastructure/interfaces';
+import { AuthPlaceholders, CoursesPlaceholders, LessonsPlaceholders, PreachingPlaceholders, RevisitsPlaceholders } from '@infrastructure/interfaces';
 
 export abstract class PlaceholdersServiceContract {
     public abstract get authPlaceholders(): AuthPlaceholders;
     public abstract get coursesPlaceholders(): CoursesPlaceholders;
-    public abstract get preachingPlaceholders(): PreachingPlaceholders;
     public abstract get lessonsPlaceholders(): LessonsPlaceholders;
+    public abstract get preachingPlaceholders(): PreachingPlaceholders;
+    public abstract get revisitsPlaceholders(): RevisitsPlaceholders;
 }
