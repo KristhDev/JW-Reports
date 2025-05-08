0363f9f7e455753309f724f9a823bf0a0c81e641
KristhDev <kristhdev@gmail.com>
2025-05-08 10:02:56 -0600

    Fix(courses): integrate translation for status messages and course title in AddOrEditCourse component

--- src/presentation/modules/courses/screens/AddOrEditCourse/AddOrEditCourse.tsx
+++ src/presentation/modules/courses/screens/AddOrEditCourse/AddOrEditCourse.tsx
@@ -3,6 +3,9 @@ import { View } from 'react-native';
 import { useStyles } from 'react-native-unistyles';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 
+/* DI */
+import { messagesService } from '@config/di';
+
 /* Components */
 import { CourseForm } from '../../components';
 import { MicrophoneBtn } from '@shared/components';
@@ -11,7 +14,7 @@ import { Title } from '@ui/components';
 /* Hooks */
 import { useCourses } from '../../hooks';
 import { useStatus } from '@shared/hooks';
-import { useUI } from '@ui/hooks';
+import { useTranslation, useUI } from '@ui/hooks';
 
 /* Theme */
 import { themeStylesheet } from '@theme/styles';
@@ -23,14 +26,22 @@ import { themeStylesheet } from '@theme/styles';
  * @return {JSX.Element} rendered component to show form to add or edit a course
  */
 const AddOrEditCourse = (): JSX.Element => {
+    const appMessages = messagesService.appMessages;
     const { styles: themeStyles, theme: { fontSizes, margins } } = useStyles(themeStylesheet);
 
     const { state: { selectedCourse } } = useCourses();
     const { setStatus } = useStatus();
+    const { translate } = useTranslation();
     const { state: { activeFormField }, setRecordedAudio } = useUI();
 
     const conditionForNotRecording = (activeFormField.trim().length === 0);
-    const handleNotRecording = () => setStatus({ code: 400, msg: 'Por favor seleccione un campo para grabar el audio.' });
+    const handleNotRecording = () => setStatus({ code: 400, msg: appMessages.SELECT_FIELD_TO_RECORD });
+
+    const title = translate('screens.courses.titles.course', {
+        action: (selectedCourse.id === '') 
+            ? translate('forms.actions.add') 
+            : translate('forms.actions.edit')
+    }).toUpperCase();
 
     return (
         <>
@@ -43,7 +54,7 @@ const AddOrEditCourse = (): JSX.Element => {
                     <Title
                         containerStyle={ themeStyles.titleContainer }
                         textStyle={{ fontSize: fontSizes.md }}
-                        text={ `${ (selectedCourse.id === '') ? 'AGREGAR' : 'EDITAR' } CURSO BÍBLICO` }
+                        text={ title }
                     />
 
                     <CourseForm />
