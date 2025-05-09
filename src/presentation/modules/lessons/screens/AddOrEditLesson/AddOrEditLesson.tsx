9679e4b860e84e087752b41864ecc1b199f210c9
KristhDev <kristhdev@gmail.com>
2025-05-09 15:00:15 -0600

    Fix(lessons): enhance localization by integrating translation for lesson titles and status messages

--- src/presentation/modules/lessons/screens/AddOrEditLesson/AddOrEditLesson.tsx
+++ src/presentation/modules/lessons/screens/AddOrEditLesson/AddOrEditLesson.tsx
@@ -3,6 +3,9 @@ import { View } from 'react-native';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-controller';
 import { useStyles } from 'react-native-unistyles';
 
+/* DI */
+import { messagesService } from '@config/di';
+
 /* Components */
 import { LessonForm } from '../../components';
 import { MicrophoneBtn } from '@shared/components';
@@ -11,7 +14,7 @@ import { Title } from '@ui/components';
 /* Hooks */
 import { useLessons } from '../../hooks';
 import { useStatus } from '@shared/hooks';
-import { useUI } from '@ui/hooks';
+import { useTranslation, useUI } from '@ui/hooks';
 
 /* Theme */
 import { themeStylesheet } from '@theme/styles';
@@ -23,18 +26,20 @@ import { themeStylesheet } from '@theme/styles';
  * @return {JSX.Element} rendered component to show form to add or edit a lesson
  */
 const AddOrEditLesson = (): JSX.Element => {
+    const appMessages = messagesService.appMessages;
     const { styles: themeStyles, theme: { fontSizes, margins } } = useStyles(themeStylesheet);
 
     const { state: { selectedLesson } } = useLessons();
     const { setStatus } = useStatus();
+    const { translate } = useTranslation();
     const { state: { activeFormField }, setRecordedAudio } = useUI();
 
     const conditionForNotRecording = (activeFormField.trim().length === 0);
-    const handleNotRecording = () => setStatus({ code: 400, msg: 'Por favor seleccione un campo para grabar el audio.' });
+    const handleNotRecording = () => setStatus({ code: 400, msg: appMessages.SELECT_FIELD_TO_RECORD });
 
     const title = (selectedLesson.id === '')
-        ? 'AGREGAR CLASE AL CURSO'
-        : 'EDITAR CLASE DEL CURSO';
+        ? translate('screens.lessons.titles.addLesson')
+        : translate('screens.lessons.titles.editLesson');
 
     return (
         <>
