9c4e22fdfc816bd11be27d1488aa856391fe839c
KristhDev <kristhdev@gmail.com>
2025-05-09 15:24:43 -0600

    Feat(services): add ThemeService for managing theme options with localization support

---
+++ src/presentation/services/theme.ts
@@ -0,0 +1,27 @@
+import { themes } from '@application/constants/utils';
+
+import { TranslationAdapterContract } from '@domain/contracts/adapters';
+import { ThemeServiceContract } from '@domain/contracts/services';
+
+export class ThemeService implements ThemeServiceContract {
+    constructor(
+        private readonly translationAdapter: TranslationAdapterContract
+    ) {}
+
+    public get THEME_OPTIONS() {
+        return [
+            { 
+                label: this.translationAdapter.translate('theme.options.lightMode'),
+                value: themes.LIGHT
+            },
+            {
+                label: this.translationAdapter.translate('theme.options.darkMode'),
+                value: themes.DARK
+            },
+            {
+                label: this.translationAdapter.translate('theme.options.defaultMode'),
+                value: themes.DEFAULT
+            }
+        ]
+    }
+}
