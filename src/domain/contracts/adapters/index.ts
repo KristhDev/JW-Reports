01d7f603b0d3c807da18f20a4c93f317aa11f98f
KristhDev <kristhdev@gmail.com>
2025-05-09 20:50:19 -0600

    Fix(contracts): ensure storage.adapter.contract is exported in index file

--- src/domain/contracts/adapters/index.ts
+++ src/domain/contracts/adapters/index.ts
@@ -3,5 +3,6 @@ export * from './external-storage.adapter.contract';
 export * from './internal-storage.adapter.contract';
 export * from './localization.adapter.contract';
 export * from './pdf.adapter.contract';
+export * from './storage.adapter.contract';
 export * from './translation.adapter.contract';
 export * from './voice-recorder.adapter.contract';
