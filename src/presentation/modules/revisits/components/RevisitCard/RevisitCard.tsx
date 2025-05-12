f8882eb5847e570ed8102d5593d413b4e363f9f1
KristhDev <kristhdev@gmail.com>
2025-05-12 04:22:43 -0600

    Refactor(revisits): convert RevisitCard to a memoized component and clean up imports

--- src/presentation/modules/revisits/components/RevisitCard/RevisitCard.tsx
+++ src/presentation/modules/revisits/components/RevisitCard/RevisitCard.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, memo, useState } from 'react';
 import { Pressable, Text, View } from 'react-native';
 import { useStyles } from 'react-native-unistyles';
 import { Menu, MenuOption, MenuOptions, MenuTrigger } from 'react-native-popup-menu';
@@ -24,22 +24,14 @@ import { Characters } from '@utils';
 import { stylesheet } from './styles';
 import { themeStylesheet } from '@theme/styles';
 
-/**
- * This component is responsible for displaying a card with the person's name and some information
- * about the revisit. It also shows a context menu with options to edit, mark as visited, pass to
- * course, or delete the revisit.
- *
- * @param {RevisitCardProps} props - The props that the component will receive.
- * @returns {JSX.Element} The rendered component.
- */
-export const RevisitCard: FC<RevisitCardProps> = ({
+export const RevisitCard = memo<RevisitCardProps>(({
     onDelete,
     onNavigateDetail,
     onNavigateEdit,
     onPass,
     onRevisit,
     revisit
-}): JSX.Element => {
+}) => {
     const [ isOpen, setIsOpen ] = useState<boolean>(false);
 
     const { styles: themeStyles } = useStyles(themeStylesheet);
@@ -178,4 +170,160 @@ export const RevisitCard: FC<RevisitCardProps> = ({
             </View>
         </Pressable>
     );
-}
+});
+
+// /**
+//  * This component is responsible for displaying a card with the person's name and some information
+//  * about the revisit. It also shows a context menu with options to edit, mark as visited, pass to
+//  * course, or delete the revisit.
+//  *
+//  * @param {RevisitCardProps} props - The props that the component will receive.
+//  * @returns {JSX.Element} The rendered component.
+//  */
+// export const RevisitCard: FC<RevisitCardProps> = ({
+//     onDelete,
+//     onNavigateDetail,
+//     onNavigateEdit,
+//     onPass,
+//     onRevisit,
+//     revisit
+// }): JSX.Element => {
+//     const [ isOpen, setIsOpen ] = useState<boolean>(false);
+
+//     const { styles: themeStyles } = useStyles(themeStylesheet);
+//     const { styles, theme: { colors, fontSizes } } = useStyles(stylesheet);
+
+//     const { setSelectedRevisit } = useRevisits();
+//     const { translate } = useTranslation();
+
+//     const nextVisit = translate('dates.visit', { date: timeAdapter.format(revisit.nextVisit, timeAdapter.formats.LOCALE_LONG_DATE) });
+//     const visitDone = translate('cards.revisits.status.done');
+
+//     /**
+//      * When the user clicks on a revisit, set the selected revisit to the revisit that was clicked on
+//      * and navigate to the RevisitDetailScreen.
+//      *
+//      * @return {void} This function does not return any value.
+//      */
+//     const handleRevisitDetail = (): void => {
+//         setSelectedRevisit(revisit);
+//         onNavigateDetail();
+//     }
+
+//     /**
+//      * When the user clicks the edit button, the modal closes, the selected revisit is set to the
+//      * current revisit, and the user is navigated to the AddOrEditRevisitScreen.
+//      *
+//      * @return {void} This function does not return any value.
+//      */
+//     const handleEdit = (): void => {
+//         setIsOpen(false);
+//         setSelectedRevisit(revisit);
+//         onNavigateEdit();
+//     }
+
+//     /**
+//      * The function takes a function as an argument and returns a function that calls the argument
+//      * function.
+//      *
+//      * @param {() => void} onAction - The function to call when the user clicks the action button.
+//      * @return {void} This function does not return any value.
+//      */
+//     const handleAction = (onAction: () => void): void => {
+//         setIsOpen(false);
+//         onAction();
+//     }
+
+//     return (
+//         <Pressable
+//             android_ripple={{
+//                 color: colors.buttonTransparent,
+//                 foreground: true
+//             }}
+//             onPress={ handleRevisitDetail }
+//             style={ styles.pressable }
+//             testID="revisit-card-pressable"
+//         >
+//             <View style={ styles.cardContainer }>
+
+//                 {/* Revisit status or date for next visit */}
+//                 <Text
+//                     style={ styles.textDate }
+//                     testID="revisit-card-next-visit-text"
+//                 >
+//                     { (revisit.done) ? visitDone : nextVisit }
+//                 </Text>
+
+//                 {/* Text person name */}
+//                 <Text
+//                     style={ styles.textName }
+//                     testID="revisit-card-person-name-text"
+//                 >
+//                     { revisit.personName }
+//                 </Text>
+
+//                 {/* Text about person */}
+//                 <Text
+//                     style={ styles.textDescription }
+//                     testID="revisit-card-about-text"
+//                 >
+//                     { Characters.truncate(revisit.about, 200) }
+//                 </Text>
+
+//                 <Fab
+//                     color="transparent"
+//                     icon={
+//                         <Ionicons
+//                             color={ colors.button }
+//                             name="ellipsis-vertical"
+//                             size={ (fontSizes.md - 3) }
+//                         />
+//                     }
+//                     onPress={ () => setIsOpen(true) }
+//                     style={ themeStyles.menuButton }
+//                     touchColor={ colors.buttonTransparent }
+//                 />
+
+//                 {/* Context menu */}
+//                 <Menu
+//                     onBackdropPress={ () => setIsOpen(false) }
+//                     opened={ isOpen }
+//                     style={ themeStyles.menuPosition }
+//                 >
+//                     <MenuTrigger text="" />
+
+//                     <MenuOptions
+//                         optionsContainerStyle={ themeStyles.menuContainer(220) }
+//                     >
+//                         <MenuOption onSelect={ handleEdit }>
+//                             <Text style={ themeStyles.menuItemText }>
+//                                 { translate('forms.actions.edit') }
+//                             </Text>
+//                         </MenuOption>
+
+//                         <MenuOption onSelect={ () => handleAction(onRevisit) }>
+//                             <Text style={ themeStyles.menuItemText }>
+//                                 { (revisit.done) 
+//                                     ? translate('cards.revisits.actions.returnToVisit') 
+//                                     : translate('cards.revisits.actions.markAsVisited') 
+//                                 }
+//                             </Text>
+//                         </MenuOption>
+
+//                         <MenuOption onSelect={ () => handleAction(onPass) }>
+//                             <Text style={ themeStyles.menuItemText }>
+//                                 { translate('cards.revisits.actions.proceedToBibleCourse') }
+//                             </Text>
+//                         </MenuOption>
+
+//                         <MenuOption onSelect={ () => handleAction(onDelete) }>
+//                             <Text style={ themeStyles.menuItemText }>
+//                                 { translate('forms.actions.delete') }
+//                             </Text>
+//                         </MenuOption>
+//                     </MenuOptions>
+//                 </Menu>
+//             </View>
+//         </Pressable>
+//     );
+// }
