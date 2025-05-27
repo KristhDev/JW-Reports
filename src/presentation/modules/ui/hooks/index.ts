3bb1b6b61f65444f6a4d31bf87a6b8e9cc022af5
KristhDev <kristhdev@gmail.com>
2025-05-26 23:48:53 -0600

    Feat(ui): add useToaster hook for managing toast notifications

--- src/presentation/modules/ui/hooks/index.ts
+++ src/presentation/modules/ui/hooks/index.ts
@@ -1,3 +1,4 @@
 export { default as useAsyncAction } from './useAsyncAction';
+export { default as useToaster } from './useToaster';
 export { default as useTranslation } from './useTranslation';
 export { default as useUI } from './useUI';
