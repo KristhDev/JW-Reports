7af01aa673de579adefbccba036f26d5128c2271
KristhDev <kristhdev@gmail.com>
2025-05-26 18:49:58 -0600

    Refactor(revisits): streamline RevisitsList component by consolidating hooks and removing unused state and props

--- src/presentation/modules/revisits/components/RevisitsList/RevisitsList.tsx
+++ src/presentation/modules/revisits/components/RevisitsList/RevisitsList.tsx
@@ -1,7 +1,7 @@
-import React, { FC, useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import { RefreshControl, View } from 'react-native';
 import { FlashList } from '@shopify/flash-list';
-import { Href, useNavigation, useRouter } from 'expo-router';
+import { Href, useRouter } from 'expo-router';
 import { useStyles } from 'react-native-unistyles';
 
 /* Entities */
@@ -9,15 +9,10 @@ import { RevisitEntity } from '@domain/entities';
 
 /* Components */
 import { RevisitCard } from '@revisits/components';
-import { ListEmptyComponent, ListFooterComponent, SearchInput, Title } from '@ui/components';
+import { Filters, ListEmptyComponent, ListFooterComponent, SearchInput } from '@ui/components';
 
 /* Hooks */
-import { useRevisits } from '@revisits/hooks';
-import { useNetwork } from '@shared/hooks';
-import { useTranslation } from '@ui/hooks';
-
-/* Interfaces */
-import { RevisitsListProps } from './interfaces';
+import { useRevisits, useRevisitsList } from '@revisits/hooks';
 
 /* Theme */
 import { themeStylesheet } from '@theme/styles';
@@ -34,93 +29,27 @@ import { themeStylesheet } from '@theme/styles';
  * - title: This string is a title of screen
  * @return {JSX.Element} Return a list of revisits
  */
-export const RevisitsList: FC<RevisitsListProps> = ({ emptyMessage, filter, title }): JSX.Element => {
-    const [ searchTerm, setSearchTerm ] = useState<string>('');
-    const [ isRefreshing, setIsRefreshing ] = useState<boolean>(false);
-
+export const RevisitsList = (): JSX.Element => {
     const router = useRouter();
-    const { styles: themeStyles, theme: { fontSizes, margins } } = useStyles(themeStylesheet);
+    const { styles: themeStyles, theme: { margins } } = useStyles(themeStylesheet);
 
-    const navigation = useNavigation();
-    const navigationState = navigation.getState();
+    const { setSelectedRevisit } = useRevisits(); 
 
     const {
-        state: {
-            hasMoreRevisits,
-            isRevisitsLoading,
-            refreshRevisits,
-            revisits,
-            revisitsScreenHistory
-        },
-        removeRevisits,
-        setRefreshRevisits,
-        setRevisitsPagination,
-        setSelectedRevisit,
-        loadRevisits,
-    } = useRevisits();
+        emptyMsg,
+        filter,
+        isRefreshing,
+        isRevisitsLoading,
+        revisits,
+        searchTerm,
+        revisitsFiltersItems,
 
-    const { wifi } = useNetwork();
-    const { translate } = useTranslation();
-
-    const noFoundResultsMsg = translate('messages.noResults', {
-        attribute: translate('entities.revisits'),
-        search: searchTerm.trim()
-    });
-
-    const emptyMsg = useMemo(
-        () => (searchTerm.trim().length > 0) ? noFoundResultsMsg : emptyMessage,
-        [ searchTerm ]
-    );
-
-    /**
-     * When the user refreshes the page, the search term is reset, the pagination is reset, the
-     * revisits are removed, and the revisits are loaded.
-     *
-     * @return {Promise<void>} This function returns nothing
-     */
-    const handleRefreshing = useCallback(async (): Promise<void> => {
-        if (isRevisitsLoading) return;
-
-        setIsRefreshing(true);
-        setSearchTerm('');
-
-        if (wifi.hasConnection) {
-            removeRevisits();
-            setRevisitsPagination({ from: 0, to: 9 });
-            await loadRevisits({ filter, refresh: true });
-        }
-
-        setIsRefreshing(false);
-    }, [ isRevisitsLoading, wifi.hasConnection, filter ]);
-
-    /**
-     * If the search string is not empty, reset the pagination, remove the revisits, load the revisits,
-     * and set the refreshing state to false.
-     *
-     * @param {string} search - string
-     * @return {Promise<void>} This function returns nothing
-     */
-    const handleSearchRevisits = useCallback(async (search: string): Promise<void> => {
-        if (isRevisitsLoading) return;
-        setSearchTerm(search);
-
-        if (wifi.hasConnection) {
-            setRevisitsPagination({ from: 0, to: 9 });
-            removeRevisits();
-            await loadRevisits({ filter, search, refresh: true });
-        }
-    }, [ isRevisitsLoading, wifi.hasConnection, filter ]);
-
-    /**
-     * If there are no more revisits to load, or if revisits are already loading, return. Otherwise,
-     * load more revisits.
-     *
-     * @return {Promise<void>} This function does not return any value
-     */
-    const handleEndReach = useCallback( async (): Promise<void> => {
-        if (!hasMoreRevisits || revisits.length === 0 || isRevisitsLoading || !wifi.hasConnection) return;
-        await loadRevisits({ filter, search: searchTerm, loadMore: true });
-    }, [ hasMoreRevisits, revisits.length, isRevisitsLoading, wifi.hasConnection, filter, searchTerm ]);
+        onEndReach,
+        onMountRevisits,
+        onRefreshingRevisits,
+        onSearchRevisits,
+        setFilter
+    } = useRevisitsList();
 
     /**
      * HandleShowModal is a function that takes a revisit and a setShowModal function as parameters and
@@ -134,24 +63,24 @@ export const RevisitsList: FC<RevisitsListProps> = ({ emptyMessage, filter, titl
     const handleShowModal = useCallback((revisit: RevisitEntity, href: Href): void => {
         setSelectedRevisit(revisit);
         router.navigate(href);
-    }, []);
+    }, [ setSelectedRevisit, router ]);
 
     const renderHeader = useCallback(() => (
-        <View style={{ paddingHorizontal: margins.xs, width: '100%' }}>
-            <Title
-                containerStyle={{ marginVertical: margins.xs }}
-                text={ title }
-                textStyle={{ fontSize: fontSizes.md }}
-            />
-
+        <View style={ themeStyles.listHeaderContainer }>
             <SearchInput
-                onClean={ () => setSearchTerm('') }
-                onSearch={ handleSearchRevisits }
+                onClean={ () => onSearchRevisits('') }
+                onSearch={ onSearchRevisits }
                 refreshing={ isRefreshing }
                 searchTerm={ searchTerm }
             />
+
+            <Filters 
+                items={ revisitsFiltersItems }
+                selectedFilter={ filter }
+                onFilterChange={ setFilter }
+            />
         </View>
-    ), [ title, searchTerm, isRefreshing ]);
+    ), [ filter, searchTerm, isRefreshing, isRevisitsLoading, revisitsFiltersItems ]);
 
     const renderListEmpty = useCallback(() => (
         <ListEmptyComponent
@@ -175,60 +104,39 @@ export const RevisitsList: FC<RevisitsListProps> = ({ emptyMessage, filter, titl
         const handleRevisit = () => handleShowModal(item, '/(app)/(tabs)/revisits/revisit-modal');
 
         return (
-            <RevisitCard
-                onDelete={ handleDelete }
-                onNavigateDetail={ handleDetail }
-                onNavigateEdit={ handleEdit }
-                onPass={ handlePass }
-                onRevisit={ handleRevisit }
-                revisit={ item }
-            />
+            <View style={{ marginHorizontal: margins.xs }}>
+                <RevisitCard
+                    onDelete={ handleDelete }
+                    onNavigateDetail={ handleDetail }
+                    onNavigateEdit={ handleEdit }
+                    onPass={ handlePass }
+                    onRevisit={ handleRevisit }
+                    revisit={ item }
+                />
+            </View>
         );
     }, []);
 
-    /**
-     * Effect to refresh revisits if isFocused is true and if
-     * refreshRevisits is true
-     */
     useEffect(() => {
-        if (!navigation.isFocused() || !refreshRevisits || !wifi.hasConnection) return;
-
-        removeRevisits();
-        loadRevisits({ filter, search: searchTerm, refresh: true });
-    }, [ refreshRevisits, navigationState?.index ]);
-
-    /**
-     * Effect to set refresh flag Revisits using revisitsScreenHistory
-     */
-    useEffect(() => {
-        const focusSubscription = navigation.addListener('focus', () => {
-            if (!navigationState) return;
-
-            const prevLast = revisitsScreenHistory[revisitsScreenHistory.length - 2];
-            const last = navigationState.routeNames[navigationState.index];
-
-            setRefreshRevisits(prevLast !== last);
-        })
-
-        return focusSubscription;
-    }, [ navigationState?.routeNames, navigationState?.index, revisitsScreenHistory ]);
+        onMountRevisits();
+    }, [ filter ]);
 
     return (
         <FlashList
             centerContent
-            contentContainerStyle={ themeStyles.listContainer }
+            contentContainerStyle={{ paddingBottom: 100 }}
             data={ revisits }
             estimatedItemSize={ 256 }
-            keyExtractor={ (item) => `${ filter }-${ item.id }` }
+            ItemSeparatorComponent={ () => <View style={{ height: margins.xs - 4 }} /> }
             ListEmptyComponent={ renderListEmpty }
             ListFooterComponent={ renderFooter }
             ListHeaderComponent={ renderHeader }
             ListHeaderComponentStyle={{ alignSelf: 'flex-start' }}
-            onEndReached={ handleEndReach }
+            onEndReached={ onEndReach }
             onEndReachedThreshold={ 0.5 }
             refreshControl={
                 <RefreshControl
-                    onRefresh={ handleRefreshing }
+                    onRefresh={ onRefreshingRevisits }
                     refreshing={ isRefreshing }
                 />
             }
