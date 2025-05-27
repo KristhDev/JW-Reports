14262c5f0fdec157cb7965176db7d4e2d92e51d3
KristhDev <kristhdev@gmail.com>
2025-05-26 18:50:15 -0600

    Refactor(preaching): update PreachingTable component to use fixed widths for table cells instead of dynamic widths

--- src/presentation/modules/preaching/components/PreachingTable/PreachingTable.tsx
+++ src/presentation/modules/preaching/components/PreachingTable/PreachingTable.tsx
@@ -1,5 +1,5 @@
 import React, { Children } from 'react';
-import { TouchableHighlight, View, useWindowDimensions } from 'react-native';
+import { TouchableHighlight, View } from 'react-native';
 import { useRouter } from 'expo-router';
 import { useStyles } from 'react-native-unistyles';
 
@@ -26,15 +26,11 @@ import { stylesheet } from './styles';
  */
 export const PreachingTable = (): JSX.Element => {
     const TABLE_PREACHING_HEADERS = publisherService.TABLE_PREACHING_HEADERS;
-    const { width } = useWindowDimensions();
 
     const { styles, theme: { colors } } = useStyles(stylesheet);
     const router = useRouter();
 
     const { state: { preachings }, setSelectedPreaching } = usePreaching();
-
-    const cellWidth = (width - 24) / 6;
-    const cellWidthHours = (width - 24) / 3;
 
     /**
      * I'm going to navigate to a screen called AddOrEditPreachingScreen, and I'm going to pass it a
@@ -59,7 +55,7 @@ export const PreachingTable = (): JSX.Element => {
                 { Children.toArray(TABLE_PREACHING_HEADERS.map((head, index) => (
                     <TableCell
                         text={ head }
-                        style={{ width: (index <= 1) ? cellWidth : cellWidthHours }}
+                        style={{ width: (index <= 1) ? '17.5%' : '32.5%' }}
                     />
                 ))) }
             </View>
@@ -74,22 +70,22 @@ export const PreachingTable = (): JSX.Element => {
                     <View style={ styles.tableRow }>
                         <TableCell
                             text={ (index + 1).toString() }
-                            style={{ backgroundColor: colors.tableRow, width: cellWidth }}
+                            style={{ backgroundColor: colors.tableRow, width: '17.5%' }}
                         />
 
                         <TableCell
                             text={ timeAdapter.format(preaching.day, timeAdapter.formats.DAY) }
-                            style={{ backgroundColor: colors.tableRow, width: cellWidth }}
+                            style={{ backgroundColor: colors.tableRow, width: '17.5%' }}
                         />
 
                         <TableCell
                             text={ timeAdapter.format(preaching.initHour, timeAdapter.formats.HOURS_MINUTES) }
-                            style={{ backgroundColor: colors.tableRow, width: cellWidthHours }}
+                            style={{ backgroundColor: colors.tableRow, width: '32.5%' }}
                         />
 
                         <TableCell
                             text={ timeAdapter.format(preaching.finalHour, timeAdapter.formats.HOURS_MINUTES) }
-                            style={{ backgroundColor: colors.tableRow, width: cellWidthHours }}
+                            style={{ backgroundColor: colors.tableRow, width: '32.5%' }}
                         />
                     </View>
                 </TouchableHighlight>
@@ -99,12 +95,12 @@ export const PreachingTable = (): JSX.Element => {
             <View style={ styles.tableRow }>
                 <TableCell
                     text="Total"
-                    style={{ backgroundColor: colors.tableFooter, width: cellWidth * 2 }}
+                    style={{ backgroundColor: colors.tableFooter, width: '35%' }}
                 />
 
                 <TableCell
                     text={ `${ timeAdapter.sumHours(preachings.map(p => ({ init: p.initHour, finish: p.finalHour }))) }H` }
-                    style={{ backgroundColor: colors.tableFooter, width: cellWidthHours * 2 }}
+                    style={{ backgroundColor: colors.tableFooter, width: '65%' }}
                 />
             </View>
         </View>
