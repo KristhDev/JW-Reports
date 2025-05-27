acc79531ef3220d5d8c0cb3a2b24f93b8bc4881b
KristhDev <kristhdev@gmail.com>
2025-05-26 18:46:04 -0600

    Refactor(ui): update styles and interface for improved layout

--- src/presentation/modules/ui/components/SearchInput/interfaces.ts
+++ src/presentation/modules/ui/components/SearchInput/interfaces.ts
@@ -1,7 +1,10 @@
+import { StyleProp, ViewStyle } from 'react-native';
+
 /* Defining the props that the component will receive. */
 export interface SearchInputProps {
     onClean: () => void;
     onSearch: (search: string) => void;
     refreshing: boolean;
     searchTerm: string;
+    style?: StyleProp<ViewStyle>;
 }
--- src/presentation/modules/ui/components/SearchInput/styles.ts
+++ src/presentation/modules/ui/components/SearchInput/styles.ts
@@ -2,21 +2,8 @@ import { createStyleSheet } from 'react-native-unistyles';
 
 export const stylesheet = createStyleSheet(({ colors, borderRadius, margins }) => ({
     searchInputContainer: {
-        alignItems: 'center',
-        flexDirection: 'row',
-        marginBottom: margins.md,
-        paddingHorizontal: margins.xs,
-        width: '100%'
+        minWidth: '100%'
     },
-
-    inputContainer: (isFocused) => ({
-        alignItems: 'center',
-        borderColor: (isFocused) ? colors.button : colors.icon,
-        borderBottomWidth: 1,
-        flex: 1,
-        flexDirection: 'row',
-        paddingLeft: (margins.xs - 4)
-    }),
 
     cleanBtn: {
         alignItems: 'center',
