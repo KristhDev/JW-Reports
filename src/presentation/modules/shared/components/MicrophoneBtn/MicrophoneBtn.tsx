92bd72a4f3b5229da6babe2590598beb907d50ce
KristhDev <kristhdev@gmail.com>
2025-05-27 21:06:56 -0600

    Fix(ui): enhance MicrophoneBtn component documentation with detailed prop descriptions

--- src/presentation/modules/shared/components/MicrophoneBtn/MicrophoneBtn.tsx
+++ src/presentation/modules/shared/components/MicrophoneBtn/MicrophoneBtn.tsx
@@ -3,8 +3,15 @@ import { Pressable } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { useStyles } from 'react-native-unistyles';
 
+/* Interfaces */
 import { MicrophoneBtnProps } from './interfaces';
 
+/**
+ * A simple button to toggle recording with a microphone icon.
+ *
+ * @param {MicrophoneBtnProps} props Component props.
+ * @returns {JSX.Element} The component.
+ */
 export const MicrophoneBtn: FC<MicrophoneBtnProps> = ({ onPress, isRecording, disabled }): JSX.Element => {
     const { theme: { colors, fontSizes } } = useStyles();
 
