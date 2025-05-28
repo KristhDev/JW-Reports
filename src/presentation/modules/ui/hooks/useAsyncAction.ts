84dcea0fd713f9029f1333f63318cb13b923fc2b
KristhDev <kristhdev@gmail.com>
2025-05-27 21:06:20 -0600

    Fix(ui): add documentation comments for useAsyncAction hook to clarify parameters and return type

--- src/presentation/modules/ui/hooks/useAsyncAction.ts
+++ src/presentation/modules/ui/hooks/useAsyncAction.ts
@@ -3,7 +3,13 @@ import { useState } from 'react';
 const useAsyncAction = <Args extends any[]>(action: (...args: Args) => Promise<void>) => {
     const [ isLoading, setIsLoading ] = useState(false);
 
-    const excuteAsyncAction = async (...args: Args) => {
+    /**
+     * Execute the async action and manage the loading state
+     * 
+     * @param {Args} args arguments of the action
+     * @return {Promise<void>} A promise that resolves when the action is completed
+     */
+    const excuteAsyncAction = async (...args: Args): Promise<void> => {
         setIsLoading(true);
         await action(...args);
         setIsLoading(false);
