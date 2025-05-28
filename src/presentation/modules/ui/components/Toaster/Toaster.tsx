f519c5c3ad36f7123661ba17526cbcdf4d8537fe
KristhDev <kristhdev@gmail.com>
2025-05-27 21:06:28 -0600

    Fix(ui): enhance Toaster component documentation with detailed prop descriptions

--- src/presentation/modules/ui/components/Toaster/Toaster.tsx
+++ src/presentation/modules/ui/components/Toaster/Toaster.tsx
@@ -4,14 +4,30 @@ import { useStyles } from 'react-native-unistyles';
 import Animated, { FadeInDown, FadeOutDown } from 'react-native-reanimated';
 import Ionicons from '@expo/vector-icons/Ionicons';
 
+/* Components */
 import { Link } from '../Link';
 
+/* Hooks */
 import { useToaster } from '@ui/hooks';
 
+/* Interfaces */
 import { ToasterProps } from './interfaces';
 
+/* Styles */
 import { stylesheet } from './styles';
 
+/**
+ * A functional component that displays a toaster notification with an optional message,
+ * confirm action, and cancel action. The toaster animates its entrance and exit.
+ *
+ * @param {ToasterProps} props - The component props.
+ * @param {ToastAction} [props.cancelAction] - The action to be performed when the cancel button is pressed.
+ * @param {ToastAction} [props.confirmAction] - The action to be performed when the confirm button is pressed.
+ * @param {string} props.message - The message to be displayed in the toaster.
+ * @param {StyleProp<ViewStyle>} [props.style] - The style to be applied to the toaster container.
+ * @param {StyleProp<TextStyle>} [props.textStyle] - The style to be applied to the toaster text.
+ * @returns {JSX.Element} The rendered toaster component.
+ */
 export const Toaster: FC<ToasterProps> = ({ cancelAction, confirmAction, message, style, textStyle }): JSX.Element => {
     const { width } = useWindowDimensions();
     const { styles, theme: { colors, fontSizes } } = useStyles(stylesheet);
